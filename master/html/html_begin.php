e17664dfaeb334d0751927ddd46c20419305cd3a
Mateusz <m.jancarz@tarendo.com>
2014-03-17 22:55:11 +0100

    Przerzucono pliki do folderu master

---
+++ master/html/html_begin.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+	<title>Egzaminy ustne</title>
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+
+<!-- 	<link rel="shortcut icon" href="img/favicon.png" type="image/x-icon"/> -->
+
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/custom.css">
+	<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+
+	<script src="js/jquery-1.9.1.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+<!--	<script src="js/custom.js"></script> -->
+    <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+
+</head>
+
+<body style="background-image: url('img/books.jpg'); padding-top: 100px;">
+
+<?php include("navbar.php"); ?>
+
+<div style="min-height: 92%;
+    height: auto !important;
+    height: 100%;
+    margin: 0 auto 0;">
+
+<div class="container col-md-8 col-md-offset-2" style="padding: 0px; padding-top:0px; margin-top: -20px;">
+
+
+
+<div class="panel col-md-12" style="margin-top: 38px; padding: 50px; background: rgba(255,255,255,0.75);">
