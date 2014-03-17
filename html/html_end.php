385db8b5ce0d54bd32eac6490fb98083934c442a
Mateusz <m.jancarz@tarendo.com>
2014-03-17 21:50:53 +0100

    Dodano szkielet strony

---
+++ html/html_end.php
@@ -0,0 +1,17 @@
+</div>
+</div>
+</div>
+<div class="container col-md-8 col-md-offset-2" style="height: 8%; padding: 4px 8px 4px; background: rgba(0,0,0,0.7);; color: #999; box-shadow: 2px -2px 20px #444444; text-align: center;
+vertical-align: middle;">
+<p class="pull-left" style="letter-spacing:1px">Mati Development ©</p>
+<span class="pull-right" style="margin-top: 1px; margin-left: 80px;">
+	<span><a id="contact" href="?">Kontakt</a></span>
+	<span>| <a id="authors" href="?">Autorzy</a> |</span>
+	<span> <a id="help" href="?">Pomoc</a></span>
+</span>
+</div>
+
+
+</body>
+
+</html>
---
+++ html/navbar.php
@@ -0,0 +1,22 @@
+    <div class="navbar navbar-inverse navbar-fixed-top" style="background: rgba(0,0,0,0.75); box-shadow: 2px 2px 20px #444444;">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">#Home</a>
+        </div>
+        <div class="collapse navbar-collapse">
+<!--           <ul class="nav navbar-nav">
+            <li><a href="?">Dodaj egzamin</a></li>
+          </ul> -->
+
+          <ul class="nav navbar-nav pull-right">
+
+            <li><a href="?" id="my_acc">Zaloguj</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
