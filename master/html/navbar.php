1288beb6efe4721693026dd939bde390567ed950
Zbigniew Rębacz <klugier@poczta.onet.pl>
2014-03-19 20:58:05 +0100

    Poprawiono dokumentację

--- master/html/navbar.php
+++ master/html/navbar.php
@@ -1,6 +1,6 @@
     <div class="navbar navbar-inverse navbar-fixed-top" style="background: rgba(0,0,0,0.75); box-shadow: 2px 2px 20px #444444;">
       <div class="container">
-        <div class="navbar-header" style="margin-left=-50px;">
+        <div class="navbar-header">
           <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
