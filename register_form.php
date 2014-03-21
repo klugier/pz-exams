fe4a9518425dde47d71506bc41b3abc1848a2ba8
Arkadiusz Koszczan <arkadiusz.koszczan@gmail.com>
2014-03-22 00:40:37 +0100

    Zmiany w register_form.php layout + instalacja Captcha
    -> captcha jest już obecna na stronie ale wymaga integracji z
       formularzem
    -> zmiany w layout zgodnie z sugestiami Michała

--- register_form.php
+++ register_form.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <?php 
 	include_once("lib/Lib.php");
 
@@ -5,12 +6,17 @@
 	$scriptsDefer = array("js/ValidateForm.js");
 	include("html/Begin.php"); 
 ?>
-
 	<div class="container"> 
 		<form class="form-horizontal" role="form">
 			<div class="form-group">
-				<h2 class="col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-xs-5 col-sm-5 col-md-5"> Zarejestruj Konto </h2>
-			</div> 
+				<div class="row">
+					<h2 class="col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-xs-5 col-sm-5 col-md-5"> Zarejestruj Konto </h2>
+				</div>
+				<div class="row">
+					<div class="col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-xs-5 col-sm-5 col-md-5">  <hr style="color: #123455;">  </div>
+				</div> 
+			</div>
+			
 			<div class="form-group">
 				<label for="email" class="col-xs-2 col-sm-2 col-md-2 control-label">E-mail </label>
 				<div class="col-xs-4 col-sm-4 col-md-4">
@@ -18,7 +24,7 @@
 				</div> 
 				<span class="help-block" id="email-error-message" style="visibility:hidden">
 						<span style="background-color:#F13333;" class="badge pull-left ">!</span>
-					  <span style="padding:5px"> Nieprawidłowy adress.</span>
+					<span style="padding:5px"> Nieprawidłowy adress.</span>
 				</span>
 			</div>
 			<div class="form-group">
@@ -58,9 +64,22 @@
 					</select>
 				</div> 
 			</div>
+			<div class="form-group" > 
+				<div class="col-xs-4 col-sm-4 col-md-4 col-xs-offset-2 col-sm-offset-2 col-md-offset-2">
+					<img id="captcha" src="/securimage/securimage_show.php"  class="img-thumbnail .img-rounded:2px" alt="CAPTCHA IMAGE" />
+				</div> 
+				<div class="col-xs-2 col-sm-2 col-md-2">
+					<div class="row">
+						<a href="#" onclick="document.getElementById('captcha').src = '/securimage/securimage_show.php?' + Math.random(); return false"> Reload Image </a>
+					</div> 
+					<div class="row">
+						<input type="text" class="form-control" name="captcha_code"  />
+					</div> 
+				</div> 
+			</div>
 			<div class="form-group">
 				<div class="col-xs-offset-2 col-sm-offset-2 col-md-offset-2 col-xs-4 col-sm-4 col-md-4">
-					<button type="submit" class="btn btn-default">Zarejestruj konto</button>
+					<button type="submit" class="btn btn-success">Zarejestruj konto</button>
 				</div>
 			</div>
 		</form>
