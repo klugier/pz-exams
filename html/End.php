0ddc93370fbb9f00202e7e99f6366ff54b446c50
Zbigniew Rębacz <klugier@poczta.onet.pl>
2014-03-21 23:18:24 +0100

    .cosmetic

--- html/End.php
+++ html/End.php
@@ -5,9 +5,9 @@
 			<p class="pull-left" style="letter-spacing:1px">PZ-Exams 2014. Wszystkie prawa zastrzeżone ©</p>
 			<span class="pull-right" style="margin-top: 1px; margin-left: 80px;">
 				<span><a id="contact" href="?">Kontakt</a></span>
-				<span>| <a id="authors" href="authors.php">Autorzy</a> |</span>
-				<span> <a id="help" href="help.php">Pomoc</a> |</span>
-				<span> <a id="help" href="register_form.php">Rejestracja</a></span>
+				<span>| <a id="authors" href="authors.php">Autorzy</a></span>
+				<span>| <a id="help" href="help.php">Pomoc</a></span>
+				<span>| <a id="help" href="register_form.php">Rejestracja</a></span>
 			</span>
 		</div>
 	</body>
