03499e74d30238317733a2861f3ff3865d3f82be
Michał P <p.michal1990@gmail.com>
2014-03-22 22:12:24 +0100

    Update register_form.php

--- register_form.php
+++ register_form.php
@@ -57,8 +57,9 @@
 			</div>  
 			<div class="form-group">
 				<label for="gender"  class="col-xs-2 col-sm-2 col-md-2 control-label"> Płeć </label>
-				<div class="col-xs-3 col-sm-3 col-md-3" >
+				<div class="col-xs-4 col-sm-4 col-md-4" >
 					<select class="form-control" id="gender">
+						<option>- Wybierz płeć -</option>
 						<option>Kobieta</option>
 						<option>Mężczyzna</option>
 					</select>
@@ -79,7 +80,7 @@
 			</div>
 			<div class="form-group">
 				<div class="col-xs-offset-2 col-sm-offset-2 col-md-offset-2 col-xs-4 col-sm-4 col-md-4">
-					<button type="submit" class="btn btn-success">Zarejestruj konto</button>
+					<button type="submit" class="btn btn-success btn-lg btn-block">Zarejestruj konto</button>
 				</div>
 			</div>
 		</form>
