c85feb7642eb39a4a9876107603f18b14eb345d2
Zbigniew Rębacz <klugier@poczta.onet.pl>
2014-03-22 02:15:19 +0100

    .cosmetic

--- html/Begin.php
+++ html/Begin.php
@@ -51,4 +51,4 @@
 		<?php include_once("Navbar.php"); ?>
 		<div style="min-height: 92%; height: auto !important; height: 100%; margin: 0 auto 0;">
 			<div class="container col-md-8 col-md-offset-2" style="padding: 0px; padding-top:0px;">
-				<div class="panel col-md-12" style="padding: 50px; background: rgba(255,255,255,0.75);">
+				<div class="panel col-md-12" style="padding: 20px; background: rgba(255, 255, 255, 0.9);">
