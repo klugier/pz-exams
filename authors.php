c6afbe864afa35b671b1cfac9ee1fbb52198c23e
Michal Szura <michal.szura@gmail.com>
2014-03-26 22:29:52 +0100

    Update podstrony Authors

--- authors.php
+++ authors.php
@@ -5,10 +5,14 @@
 	include("html/Begin.php");
 ?>
 
-<div class="container"> 
+<div class="container" id="authors"> 
 	<ul>
 		<li><b>Project Owner:</b> <ul><li>Zbigniew Rębacz</li></ul></li>
+    </ul>
+    <ul>
 		<li><b>SCRUM Master:</b> <ul><li>Michał Pachel</li></ul></li>
+    </ul>
+    <ul>
 		<li><b>Contributors:</b><ul>
 			<li>Arkadisz Koszczan</li>
 			<li>Michał Szura</li>
