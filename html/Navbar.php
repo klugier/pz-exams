ebfa4f57bb7732df658a9e03c4c554c6156058bf
Michał P <p.michal1990@gmail.com>
2014-03-27 02:09:40 +0100

    nazwanie pól formularza logowania

--- html/Navbar.php
+++ html/Navbar.php
@@ -31,10 +31,10 @@
 				    <!--<button type="submit" class="btn btn-danger">Wyloguj</button>-->
                     <ul class="dropdown-menu" style="width:300px; background: rgba(255,255,255,0.9);-webkit-border-radius: 5px; -khtml-border-radius: 5px;-moz-border-radius: 5px; border-radius: 5px; margin-top:8px;">
                         <li> 
-                  	    <form class="form-signin" role="form" style="margin-right:10px;margin-left:10px;" method="post" action="user_panel.php">
+                  	    <form class="form-signin" role="form" style="margin-right:10px;margin-left:10px;" method="post" action="login.php">
       				<h3 style="text-align:center; font-weight:bold; padding-bottom:15px;">Logowanie do systemu</h3>
-      				<input type="email" class="form-control" placeholder="adres email" required autofocus style="margin-bottom:3px;">
-      				<input type="password" class="form-control" placeholder="hasło" required>
+      				<input type="email" name="email" class="form-control" placeholder="adres email" required autofocus style="margin-bottom:3px;">
+      				<input type="password" name="pass" class="form-control" placeholder="hasło" required>
       				<label class="checkbox">
       				<input type="checkbox" value="remember-me"> Zapamiętaj mnie
       				</label>
