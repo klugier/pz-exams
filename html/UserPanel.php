5498c783272450da0836ebe5d971921eab3c58a6
Michał P <p.michal1990@gmail.com>
2014-04-01 22:38:11 +0200

    przeniesienie menu egzaminatora do folderu html

---
+++ html/UserPanel.php
@@ -0,0 +1,9 @@
+
+   <div class="navbar navbar-inverse " style="background: rgba(0,0,0,0.75); box-shadow: 2px 2px 20px #444444;">
+
+          <a class="navbar-brand" href="AddExam.php" id="user_m" >Dodaj egzamin</a>
+          <a class="navbar-brand" href="user_panel.php" id="user_m">Lista egzaminów</a>
+          <a class="navbar-brand" href="user_panel.php?ed=0" id="user_m">Edytuj dane</a>
+
+   </div>
+
