0f097b76e92976613ccaaa457795572c3e7240cc
Michal Szura <michal.szura@gmail.com>
2014-04-02 21:07:49 +0200

    Authors, początek UserEdit, fix UserPanel i 404

--- Authors.php
+++ Authors.php
@@ -5,21 +5,27 @@
 	include("html/Begin.php");
 ?>
 
-<div class="container" id="authors"> 
+<div class="container"> 
 	<ul>
-		<li><b>Project Owner:</b> <ul><li>Zbigniew Rębacz</li></ul></li>
+		<li id="author"><b>Project Owner:</b>
+            <ul id="creator"><li>Zbigniew Rębacz</li></ul>
+        </li>
     </ul>
     <ul>
-		<li><b>SCRUM Master:</b> <ul><li>Michał Pachel</li></ul></li>
+		<li id="author"><b>SCRUM Master:</b>
+            <ul id="creator"><li>Michał Pachel</li></ul>
+        </li>
     </ul>
     <ul>
-		<li><b>Contributors:</b><ul>
-			<li>Arkadisz Koszczan</li>
-			<li>Michał Szura</li>
-			<li>Michał Gawryluk</li>
-			<li>Mateusz Jancarz</li>
-			<li>Konrad Welc</li>
-		</ul></li>
+		<li id="author"><b>Contributors:</b>
+            <ul id="creator">
+			    <li>Arkadisz Koszczan</li>
+			    <li>Michał Szura</li>
+			    <li>Michał Gawryluk</li>
+			    <li>Mateusz Jancarz</li>
+			    <li>Konrad Welc</li>
+		    </ul>
+        </li>
 	</ul>
 </div>
 
--- html/UserPanel.php
+++ html/UserPanel.php
@@ -2,8 +2,8 @@
    <div class="navbar navbar-inverse " style="background: rgba(0,0,0,0.75); box-shadow: 2px 2px 20px #444444;">
 
           <a class="navbar-brand" href="AddExam.php" id="user_m" >Dodaj egzamin</a>
-          <a class="navbar-brand" href="user_panel.php" id="user_m">Lista egzaminów</a>
-          <a class="navbar-brand" href="user_panel.php?ed=0" id="user_m">Edytuj dane</a>
+          <a class="navbar-brand" href="404.php" id="user_m">Lista egzaminów</a>
+          <a class="navbar-brand" href="UserEdit.php" id="user_m">Edytuj dane</a>
 
    </div>
 
