dd6cc270a0bb4d7902ad9cc6b6d083c96f1d3d6d
Michal Szura <michal.szura@gmail.com>
2014-04-02 23:19:29 +0200

    Dropbne poprawki Authors

--- Authors.php
+++ Authors.php
@@ -1,35 +1,32 @@
 <?php
-	include("lib/Lib.php");
-	
-	$title = "$appName - Autorzy projektu";
-	include("html/Begin.php");
+    include("lib/Lib.php");
+    
+    $title = "$appName - Autorzy projektu";
+    include("html/Begin.php");
 ?>
 
 <div class="container">
     <h1 style="color:#ACACAC; text-shadow: 1px 1px #838996;"><b>Autorzy projektu</b></h1>
-	<ul>
-		<li id="author"><b>Project Owner:</b>
-            <ul id="creator"><li>Zbigniew Rębacz</li></ul>
-        </li>
-    </ul>
-    <br/>
-    <ul>
-		<li id="author"><b>SCRUM Master:</b>
-            <ul id="creator"><li>Michał Pachel</li></ul>
-        </li>
-    </ul>
-    <br/>
-    <ul>
-		<li id="author"><b>Contributors:</b>
-            <ul id="creator">
-			    <li>Arkadisz Koszczan</li>
-			    <li>Michał Szura</li>
-			    <li>Michał Gawryluk</li>
-			    <li>Mateusz Jancarz</li>
-			    <li>Konrad Welc</li>
-		    </ul>
-        </li>
-	</ul>
+    <hr id="author" />
+    <span id="author"><b>Project Owner:</b></span>
+    <br />
+    <span id="creator">Zbigniew Rębacz</span>
+    <hr id="author" />
+    <span id="author"><b>SCRUM Master:</b></span>
+    <br />
+    <span id="creator">Michał Pachel</span>
+    <hr id="author" />
+    <span id="author"><b>Contributors:</b></span>
+    <br />
+    <span id="creator">Arkadisz Koszczan</span>
+    <br />
+    <span id="creator">Michał Szura</span>
+    <br />
+    <span id="creator">Michał Gawryluk</span>
+    <br />
+    <span id="creator">Mateusz Jancarz</span>
+    <br />
+    <span id="creator">Konrad Welc</span>
 </div>
 
 <?php include ("html/End.php"); ?>
