c35707664b20b217ed29430cf11efc6d67e6e7de
Zbigniew Rębacz <klugier@poczta.onet.pl>
2014-04-05 23:50:41 +0200

    .cosmetics

--- ExamList.php
+++ ExamList.php
@@ -15,55 +15,54 @@
 		echo "<div class=\"alert alert-info\">";
 		echo "<b>Nie dodałeś jeszcze żadnych egzaminów!</b> Zobacz jakie to proste i <u><b><a href=\"AddExam.php\">utwórz</a></b></u> swój pierwszy egzamin.";
 		echo "</div>";
-		return;
-	}
-	
-	echo '
-	<table class="table">
-		<thead>
-			<tr>
-				<th><center>Lp.</center></th>
-				<th>Nazwa</th>
-				<th><center>Aktywowany</center></th>
-				<th><center>Operacje</center></th>
-			</tr>
-		</thead>
-		<tbody>
-	';
 		
-	echo "<tr>\n";
-	$i = 1;
-	foreach ($exams as $exam) {
-		echo "<tr>";
-		echo "<td><center>" . $i . ".</center></td>\n";
-		echo "<td>" . $exam->getName() . "</td>\n";
-			
-		// Activated
-		echo "<td>";
-		if ($exam->getActivated()) {
-			echo "<center>Tak</center>";
-		}
-		else {
-			echo "<center>Nie</center>";
-		}
-		echo "</td>";
-			
-		// Options
-		echo "<td><center>" .
-			 "<i class=\"glyphicon glyphicon-pencil\" style=\"margin-right: 10px;\"></i>" .
-			 "<i class=\"glyphicon glyphicon-remove\"></i>" .
-			 "</center></td>";
-			
-		echo "</tr>";
+	} else {
+		echo '
+		<table class="table">
+			<thead>
+				<tr>
+					<th><center>Lp.</center></th>
+					<th>Nazwa</th>
+					<th><center>Aktywowany</center></th>
+					<th><center>Operacje</center></th>
+				</tr>
+			</thead>
+			<tbody>
+		';
 		
-		$i++;
-	}
-	echo "</tr>\n";
+		echo "<tr>\n";
+		$i = 1;
+		foreach ($exams as $exam) {
+			echo "<tr>";
+			echo "<td><center>" . $i . ".</center></td>\n";
+			echo "<td>" . $exam->getName() . "</td>\n";
+			
+			// Activated
+			echo "<td>";
+			if ($exam->getActivated()) {
+				echo "<center>Tak</center>";
+			} else {
+				echo "<center>Nie</center>";
+			}
+			echo "</td>";
+			
+			// Options
+			echo "<td><center>" .
+				"<i class=\"glyphicon glyphicon-pencil\" style=\"margin-right: 10px;\"></i>" .
+				"<i class=\"glyphicon glyphicon-remove\"></i>" .
+				"</center></td>";
+			
+			echo "</tr>";
+		
+			$i++;
+		}
+		echo "</tr>\n";
 	
-	echo '
-		<tbody>
-	</table>
-	';
+		echo '
+			<tbody>
+		</table>
+		';
+	}
 
 	include("html/End.php");
 ?>
