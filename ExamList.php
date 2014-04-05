432286b0e55c93cb1cabd5a313a6fc27495108b1
Zbigniew Rębacz <klugier@poczta.onet.pl>
2014-04-05 14:13:14 +0200

    Dodanie pliku ExamList.php i zbindowanie go z istniejącą infrastrukturą

---
+++ ExamList.php
@@ -0,0 +1,10 @@
+<?php
+	include_once("lib/Lib.php");
+	$title = "$appName - Lista egzaminów";
+	include("html/Begin.php");
+	include("html/UserPanel.php");
+?>
+
+<?php
+	include("html/End.php");
+?>
