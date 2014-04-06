2f801b7561f3b9e6280b3365d335a4cb7da70db3
Michal Szura <michal.szura@gmail.com>
2014-04-07 00:37:04 +0200

    Update

--- UserEdit.php
+++ UserEdit.php
@@ -1,13 +1,114 @@
 <?php
     include_once("lib/Lib.php");
-	$title = "$appName - Edycja Danych";
-	include("html/Begin.php");
-	include("html/UserPanel.php");
+    
+    if ($_SESSION['USER'] == "") {
+        header('Location: index.php' ); 
+    }
+    
+    $title = "$appName - Edycja Danych";
+    $scriptsDefer = array("js/ValidateRegisterForm.js");
+    include("html/Begin.php");
+    include("html/UserPanel.php");
 ?>
 
-<ul class="nav nav-pills nav-stacked" style="width:200px; background-color:grey; border-radius:5px;">
-	<h3 style="display:block; border-bottom:3px solid; text-align:center; margin-bottom:0px;font-weight:bold;">Menu edycji</h3>
-	<li><a href="UserEdit.php?ed=1" id="test" style="color:blue; font-weight:bold;">Zmiana imienia</a></li>
-	<li><a href="UserEdit.php?ed=2" id="test" style="color:blue; font-weight:bold;">Zmiana nazwiska</a></li>
-	<li><a href="UserEdit.php?ed=3" id="test" style="color:blue; font-weight:bold;">Zmiana hasła</a></li>
-</ul>
+<form class="form-horizontal" role="form" id="register_form" method="post" action="php/HandlingRegisterForm.php">
+    <fieldset>
+        <legend>Menu edycji</legend>
+        <div class="form-group">
+            <label for="passwd" class="col-xs-2 col-sm-2 col-md-2 control-label"> Hasło </label>
+            <div class="col-xs-4 col-sm-4 col-md-4">
+                <input type="password" required class="form-control inputPassword" id="passwd" name="passwd" placeholder="Wprowadź Haslo" title="">
+            </div>
+            <span class="help-block" id="passwd-error-message">
+
+            </span>
+        </div>
+        <div class="form-group">
+            <label for="passwd-repat" class="col-xs-2 col-sm-2 col-md-2 control-label"> Potwierdź hasło </label>
+            <div class="col-xs-4 col-sm-4 col-md-4">
+                <input type="password" required class="form-control inputPassword" id="passwd-repeat" placeholder="Powtórz Haslo" title="">
+            </div>
+            <span class="help-block" id="passwd-repat-error-message">
+
+            </span>
+        </div>
+        <div class="form-group">
+            <div class="row">
+                <span class="col-xs-offset-2 col-sm-offset-2 col-md-offset-2 col-xs-3 col-sm-3 col-md-3">
+                    <button type="submit" class="btn btn-success btn-lg btn-block" name="submitButton" value="submit">Zmień  Hasło</button>
+                </span>
+                <span class="col-xs-2 col-sm-2 col-md-2">
+                    <a href="php/HandlingRegisterForm.php">
+                        <button type="button" class="btn btn-lg btn-primary btn-block" name="clearButton">Wyczyść</button>
+                    </a>
+                </span>
+            </div>
+
+        </div>
+        <br />
+        <div class="form-group">
+            <label for="firstname" class="col-xs-2 col-sm-2 col-md-2 control-label">Imię</label>
+            <div class="col-xs-4 col-sm-4 col-md-4">
+                <input type="text" class="form-control" id="firstname" placeholder="Wprowadź Imię" name="name" value="<?php if(isset($_SESSION['name'])){ echo $_SESSION['name']; } else { echo '';  }?>">
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="row">
+                <span class="col-xs-offset-2 col-sm-offset-2 col-md-offset-2 col-xs-3 col-sm-3 col-md-3">
+                    <button type="submit" class="btn btn-success btn-lg btn-block" name="submitButton" value="submit">Zmień  Imię</button>
+                </span>
+                <span class="col-xs-2 col-sm-2 col-md-2">
+                    <a href="php/HandlingRegisterForm.php">
+                        <button type="button" class="btn btn-lg btn-primary btn-block" name="clearButton">Wyczyść</button>
+                    </a>
+                </span>
+            </div>
+
+        </div>
+        <div class="form-group">
+            <label for="lastname" class="col-xs-2 col-sm-2 col-md-2 control-label">Nazwisko</label>
+            <div class="col-xs-4 col-sm-4 col-md-4">
+                <input type="text" class="form-control" id="lastname" placeholder="Wprowadź Nazwisko" name="surname" value="<?php if(isset($_SESSION['surname'])){ echo $_SESSION['surname']; } else { echo '';  }?>">
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="row">
+                <span class="col-xs-offset-2 col-sm-offset-2 col-md-offset-2 col-xs-3 col-sm-3 col-md-3">
+                    <button type="submit" class="btn btn-success btn-lg btn-block" name="submitButton" value="submit">Zmień  Nazwisko</button>
+                </span>
+                <span class="col-xs-2 col-sm-2 col-md-2">
+                    <a href="php/HandlingRegisterForm.php">
+                        <button type="button" class="btn btn-lg btn-primary btn-block" name="clearButton">Wyczyść</button>
+                    </a>
+                </span>
+            </div>
+
+        </div>
+        <div class="form-group">
+            <label for="gender" class="col-xs-2 col-sm-2 col-md-2 control-label"> Płeć </label>
+            <div class="col-xs-4 col-sm-4 col-md-4">
+                <select class="form-control" id="gender" name="gender">
+                    <option>- Wybierz płeć -</option>
+                    <option>Kobieta</option>
+                    <option>Mężczyzna</option>
+                </select>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="row">
+                <span class="col-xs-offset-2 col-sm-offset-2 col-md-offset-2 col-xs-3 col-sm-3 col-md-3">
+                    <button type="submit" class="btn btn-success btn-lg btn-block" name="submitButton" value="submit">Zmień  Płeć</button>
+                </span>
+                <span class="col-xs-2 col-sm-2 col-md-2">
+                    <a href="php/HandlingRegisterForm.php">
+                        <button type="button" class="btn btn-lg btn-primary btn-block" name="clearButton">Wyczyść</button>
+                    </a>
+                </span>
+            </div>
+
+        </div>
+    </fieldset>
+</form>
+
+<?php include ("html/End.php"); ?>
