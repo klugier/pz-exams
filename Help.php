fc0c55db858a688294c63ad3a292723b3c3caa51
Zbigniew Rębacz <klugier@poczta.onet.pl>
2014-04-06 18:53:29 +0200

    Dodanie do pomocy listy obsługiwanych dome

--- Help.php
+++ Help.php
@@ -6,7 +6,21 @@
 ?> 
 
 <div class="container"> 
-	Help should be here!
-</div>  
+	<h2>Obsługiwane domeny</h2>
+	W chwili obecnej obsługujemy następujące domeny: <br />
+	<ul>
+		<?php
+			$domains = Settings::getDomains();
+			if ($domains == null) {
+				echo "<li><b>Aktualnie obsługiwane są wszystkie domenu</b></li>";
+			} else {
+				foreach ($domains as $domain) {
+					echo "<li>" . $domain . "</li>\n";
+				}
+			}
+		?>
+	</ul>
+	
+</div>
 
 <?php include ("html/End.php"); ?>
