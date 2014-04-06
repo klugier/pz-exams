ddac6affaad4f4e54c17671323d2a71e621f7731
Arkadiusz Koszczan <arkadiusz.koszczan@gmail.com>
2014-04-06 20:35:22 +0200

    merge - obsluga kontrolki kalendarza z klasą do zarządzania danymi w zakładce dodaj egzamin

--- js/CalendarManager.js
+++ js/CalendarManager.js
@@ -1,53 +1,98 @@
 jQuery( document ).ready(function( $ ) {
 
-	function createDateKey ( date , separator ) { 
-		stringDate = ((dateStart.getDate().length === 2 ) ?  dateStart.getDate() : ( "0" + dateStart.getDate() ) ) + separator + 
-					 ((dateStart.getMonth().length === 2 ) ?  dateStart.getMonth() : ( "0" + dateStart.getMonth() ) ) + separator + 
-					 dateStart.getFullYear() ; 
-					 
-		//alert  ( stringDate) ; 
-		return stringDate ;   
-	}  
+	// klasa wymiany danych na kartach dodaj egzamin 
 	
-	function ExamUnit (  date , duration  ) { 
-		this.date = date ;  
-		this.duration = duration  ;
-	}  
+	function converToMinutes(s) {
+		var c = s.split(':');
+		return parseInt(c[0]) * 60 + parseInt(c[1]);
+	}
 
-	function CalendarDayControl ( examUnits) { 
+	function parseTime(s) {
+		if (parseInt(s) % 60 == 0){
+			return Math.floor(parseInt(s) / 60) + ":" + "00";
+		} else {
+			return Math.floor(parseInt(s) / 60) + ":" + parseInt(s) % 60;
+		}
+	}
+
+	function ExamUnit(bHour, eHour){
+		this.bHour = bHour;
+		this.eHour = eHour;
+	}
+	function Exam(name, durration){
+		this.name = "name";
+		this.durration = durration;
+		this.day = new Array();
+		this.blockedUnits = new Array();
+
+		this.addTerm = function (date, begHour, endHour, durat) {
+			var bHour = converToMinutes(begHour);
+			var eHour = converToMinutes(endHour);
+			var diff = eHour - bHour;
+			var count = diff / durat;
+			for (var i=0;i<count;i++){
+				
+				if(this.day[date] === undefined){
+					this.day[date] = new Array();
+				}
+				var conv1 = parseTime(bHour + durat * i);      
+				var conv2 = parseTime(bHour + durat * (i + 1));
+				var ExmUnit = new ExamUnit(conv1, conv2);
+				this.day[date].push(ExmUnit);
+			}
+		};
+
+		this.delTerm = function(date) {
+			delete this.day[date];
+		};
+		
+		this.blockUnit = function(date, fromHour, toHour) {
+			if(this.blockedUnits[date] === undefined) {
+				this.blockedUnits[date] = new Array();
+			}
+			this.blockedUnits[date].push(fromHour);
+			this.blockedUnits[date].push(fromHour);
+		};
+
+		this.sortExamUnit = function(date){
+			var tmpB = new Array();
+			var tmpE = new Array();
+
+			for (var k in this.day[date]){
+				
+				tmpB.push(converToMinutes(this.day[date][k].bHour));
+				tmpE.push(converToMinutes(this.day[date][k].eHour));            
+			}
+			tmpB.sort();
+			tmpE.sort();
+			delete this.day[date];
+			this.day[date] = new Array();       
+			for( var i in tmpB ){
+				var exmUnit = new ExamUnit(parseTime(tmpB[i]), parseTime(tmpE[i]));
+				this.day[date].push(exmUnit);
+			}
+		};
+	}    
+	
+	
+	function CalendarDayControl ( day , examUnits) { 
 		this.printControl = function ( ) { 
 			
 			if ( examUnits.length == 0 ) { 
 				return ; 
 			} 
-
-			blockDate = createDateKey ( examUnits[0].date ,  '-' ); 
-			htmlControl = this.controlStyleBegin ( blockDate ); 
+ 
+			htmlControl = this.controlStyleBegin ( day ); 
 			
 			for ( var item in examUnits )  { 
-				htmlControl += this.controlAddExamUnit( this.prepareTimeString ( examUnits[item].date  ) , this.prepareTimeString ( this.addDurationToDate ( examUnits[item].date, examUnits[item].duration ) ));
+				htmlControl += this.controlAddExamUnit(  examUnits[item].bHour , examUnits[item].eHour );
 			} 
 			htmlControl += this.controlAddSeparator () ;
-			
-			htmlControl += this.controlAddExamUnit ( "20:13" , "12:20" ) ; 
-			htmlControl += this.controlAddExamUnit ( "20:13" , "12:20" ) ;
-			htmlControl += this.controlAddExamUnit ( "20:13" , "12:20" ) ;
-			htmlControl += this.controlAddExamUnit ( "20:13" , "12:20" ) ;
-			
 			
 			htmlControl += this.controlStyleEnd () ;
 		    // document.write ( htmlControl  ); 
 			//$("#calendar-control").html(htmlControl);
 		} ;  
-		
-		this.prepareTimeString = function ( date , duration ) {
-			stringTime = date.getHours() + ":" + date.getMinutes(); 
-			return stringTime ; 
-		} ; 
-		
-		this.addDurationToDate = function( date , duration ) {
-			return new Date(date.getTime() + duration * 60000 ) ; 
-		} ; 
 		
 		this.controlStyleBegin = function ( date) {
 			//alert ( "height : " + this.height) ; 
@@ -98,38 +143,24 @@ jQuery( document ).ready(function( $ ) {
 	CalendarDayControl.prototype.height = 400; 
 
 
-	function CalendarControl ( )  { 
-		this.examDays = new Array () ; 
-		this.addExamBlock = function ( dateStart , dateEnd , duration  ) { 
-        	 numOfExams = (  (dateEnd.getHours() * 60 + dateEnd.getMinutes()) - (dateStart.getHours() *60 + dateStart.getMinutes()) ) / duration ;
-			 //alert( numOfExams ); 
-			 for (var i=0;i<numOfExams;i++) {
-				dateKey = createDateKey ( dateStart , "-" )  ;
-				//alert(dateKey); 
-				if( this.examDays[dateKey] === undefined ) {
-					this.examDays[dateKey] = new Array();
-				}
-				
-				examUnit = new ExamUnit ( new Date(dateStart.getTime() + i * duration *60000) , duration ) ; 
-				this.examDays[dateKey].push(examUnit) ;
-			}  
-			//alert ( this.examDays.length ) ;
-		} ;	
-
+	function CalendarControl ( examDays )  { 
 		this.printCalendar = function ( ) 	{ 
 			//alert ( this.examDays.length ) ; 
-			for(var day in this.examDays)
+			for(var day in examDays)
     		{
-				calendarDayControl = new CalendarDayControl (this.examDays[day]) ;
+				calendarDayControl = new CalendarDayControl ( day , examDays[day]) ;
 				calendarDayControl.printControl(); 
 			} 
 		} ; 
 	} 
 	
 	// test
-	dateStart = new Date ( 2006, 9, 1, 12, 20, 0 , 0 ) ;
-	dateEnd = new Date ( 2006, 9, 1, 14, 20, 0 , 0 ) ;  
-	calendarControl = new CalendarControl() ; 
-	calendarControl.addExamBlock( dateStart , dateEnd , 30 ) ;   
+	name = "egzamin z WOS " ; 
+	exam = new Exam ( name , duration ) ; 
+	exam.addTerm( "21.02.03" , "10:20", "13:20", 30) ; 
+	
+	exam.addTerm( "21.02.03" , "14:20", "15:20", 30) ;
+	
+	calendarControl = new CalendarControl(exam.day) ;    
 	calendarControl.printCalendar() ; 
 } ); 
