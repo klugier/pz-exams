746e314e0a62ee7a7b22ccd00d515fb09f80e0d4
Zbigniew Rębacz <klugier@poczta.onet.pl>
2014-04-06 15:09:50 +0200

    Rozbudowano klasę Settings o możliwość przechowywania doman

--- lib/Ajax/AjaxUserRequest.php
+++ lib/Ajax/AjaxUserRequest.php
@@ -1,27 +1,27 @@
 <?php
-include_once("../../lib/Lib.php");
-header("content-type:application/json");
-$emailExistJSON = array( 'status' => 'dataRecived' , 
-				   'email' => 'existsInDB' ) ;
+	include_once("../../lib/Lib.php");
+	header("content-type:application/json");
+	$emailExistJSON = array( 'status' => 'dataRecived' , 
+					'email' => 'existsInDB' ) ;
 	
-$emailNotInDBJSON = array( 'status' => 'dataRecived' , 
-				   'email' => 'notInDB' ) ;	
+	$emailNotInDBJSON = array( 'status' => 'dataRecived' , 
+					'email' => 'notInDB' ) ;	
 				   
 				   
-//$req = array( 'status' => $_POST['email'] , 
-//				   'email' => 'notInDB' ) ;					   
-//echo json_encode($req) ;
+	//$req = array( 'status' => $_POST['email'] , 
+	//				   'email' => 'notInDB' ) ;					   
+	//echo json_encode($req) ;
 				   
-if (isset($_POST['email'])) {
-	$basicUser = new BasicUser();
-	$basicUser -> setEmail($_POST['email']) ;  
-	if (UserDatabase::checkEmail($basicUser)) { 				   
-		echo json_encode($emailExistJSON) ;
+	if (isset($_POST['email'])) {
+		$basicUser = new BasicUser();
+		$basicUser->setEmail($_POST['email']) ;  
+		if (UserDatabase::checkEmail($basicUser)) { 				   
+			echo json_encode($emailExistJSON) ;
+		} else { 
+			echo json_encode($emailNotInDBJSON) ; 
+		} 
 	} else { 
 		echo json_encode($emailNotInDBJSON) ; 
-	} 
-} else { 
-	echo json_encode($emailNotInDBJSON) ; 
-} 
-
-?> 
+	}
+	
+?>
--- lib/General/Settings.php
+++ lib/General/Settings.php
@@ -12,31 +12,64 @@ final class Settings
 		return self::getInstance()->debug;
 	}
 	
+	public static function getDomains()
+	{
+		return self::getInstance()->domains;
+	}
+	
 	public static function setDebug($debug)
 	{
 		self::getInstance()->debug = $debug;
 	}
 	
+	public static function setDomains($domains)
+	{
+		self::getInstance()->domains = $domains;
+	}
+	
 	private static function getInstance()
 	{
+		$cfgPath = dirname(__FILE__) . "/../../cfg/Settings.xml";
+		
 		if (self::$instance == false) {
-			self::$instance = new Settings(dirname(__FILE__) . "/../../cfg/Settings.xml");
+			self::$instance = new Settings($cfgPath);
 		}
 		return self::$instance;
 	}
 	
 	private function __construct($cfgPath)
 	{
+		$this->debug   = 0;
+		$this->domains = null;
+		
+		$this->__load($cfgPath);
+	}
+	
+	private function __load($cfgPath)
+	{
 		if (!file_exists($cfgPath)) {
 			echo "Nie udało się odnaleźść pliku \"" . $cfgPath . "\".\n";
 			return;
 		}
 		
+		$dom = new DOMDocument();
+		$dom->load($cfgPath);
 		$xml = simplexml_load_file($cfgPath);
-		$this->debug = $xml->Debug;
+		
+		if ($dom->getElementsByTagName("Debug")->length > 0) {
+			$this->debug = $xml->Debug;
+		}
+		
+		if ($dom->getElementsByTagName("Domains")->length > 0) {
+			$i = 0;
+			foreach ($xml->Domains->Domain as $domain) {
+				$this->domain[$i] = $domain;
+			}
+		}
 	}
 	
 	private $debug;
+	private $domains;
 	
 	private static $instance = false;
 }
