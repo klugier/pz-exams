9744eb965d528692e727514cf48b54e828e6d234
Michał P <p.michal1990@gmail.com>
2014-04-07 21:54:40 +0200

    dodanie linkow do plikow bootstrap-datetime

--- html/Begin.php
+++ html/Begin.php
@@ -15,8 +15,13 @@
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="css/custom.css">
 		<link rel="stylesheet" type="text/css" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
+		<link href="css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
+		
 		<!-- włączenie jQuery ma być tutaj inaczej nie dziala pod operą --> 
 		<script language="javascript" type="text/javascript" src="js/Lib/jquery-1.11.0.min.js"></script>
+		
+		<script type="text/javascript" src="js/Lib/bootstrap-datetimepicker.js" charset="UTF-8"></script>
+		<script type="text/javascript" src="js/Lib/bootstrap-datetimepicker.pl.js" charset="UTF-8"></script>
 		<?php
 			if (isset($csses)) {
 				foreach ($csses as $value) {
