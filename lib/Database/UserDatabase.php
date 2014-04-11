4250253770bdfc07113318d2c1427f4eba15acf0
Konrad Welc <konrad1331@gmail.com>
2014-04-11 18:08:45 +0200

    Zbigniew nie psuj kodu, fix UserDatabase

--- lib/Database/UserDatabase.php
+++ lib/Database/UserDatabase.php
@@ -147,7 +147,7 @@
             {
                 $sql =  "UPDATE Users SET Activated = 1 WHERE Email = '$email'";
     
-                return Data baseConnector::getConnection()->query($sql) ? true : false;
+                return DatabaseConnector::getConnection()->query($sql) ? true : false;
             }
             else
             {
