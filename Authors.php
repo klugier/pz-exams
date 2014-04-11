93cf9a89df93e6985007190f79636b52518cd088
Michal Szura <michal.szura@gmail.com>
2014-04-11 18:29:11 +0200

    Authors & Arkadisz Fix

--- Authors.php
+++ Authors.php
@@ -1,24 +1,24 @@
 <?php
-    include("lib/Lib.php");
-    
-    $title = "$appName - Autorzy projektu";
-    include("html/Begin.php");
+	include("lib/Lib.php");
+	
+	$title	=	"$appName - Autorzy projektu";
+	include("html/Begin.php");
 ?>
 
-<div class="container">
-    <center>
-        <img src="img/logo_button.png">
-    </center>
-    <div id="author" style="color:#ACACAC; text-shadow: 1px 1px #838996;">Product Owner</div>
-    <div id="creatorFirst">Zbigniew Rębacz</div>
-    <div id="author" style="color:#ACACAC; text-shadow: 1px 1px #838996;">Scrum Master</div>
-    <div id="creatorFirst">Michał Pachel</div>
-    <div id="author" style="color:#ACACAC; text-shadow: 1px 1px #838996;">Development Team</div>
-    <div id="creatorFirst">Arkadisz Koszczan</div>
-    <div id="creator">Michał Szura</div>
-    <div id="creator">Michał Gawryluk</div>
-    <div id="creator">Mateusz Jancarz</div>
-    <div id="creator">Konrad Welc</div>
+<div	class="container">
+	<center>
+		<img	src="img/logo_button.png">
+	</center>
+	<div	id="author"	style="color:#ACACAC;	text-shadow:	1px	1px	#838996;">Product Owner</div>
+	<div	id="creatorFirst">Zbigniew Rębacz</div>
+	<div	id="author"	style="color:#ACACAC;	text-shadow:	1px	1px	#838996;">Scrum Master</div>
+	<div	id="creatorFirst">Michał Pachel</div>
+	<div	id="author"	style="color:#ACACAC;	text-shadow:	1px	1px	#838996;">Development Team</div>
+	<div	id="creatorFirst">Arkadiusz Koszczan</div>
+	<div	id="creator">Michał Szura</div>
+	<div	id="creator">Michał Gawryluk</div>
+	<div	id="creator">Mateusz Jancarz</div>
+	<div	id="creator">Konrad Welc</div>
 </div>
 
-<?php include ("html/End.php"); ?>
+<?php	include	("html/End.php");	?>
