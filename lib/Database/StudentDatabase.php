c64c75cdf5fa8a8702f3a37c949317ba44912cd3
Zbigniew Rębacz <klugier@poczta.onet.pl>
2014-04-11 17:10:45 +0200

    .wcięcia

--- lib/Database/StudentDatabase.php
+++ lib/Database/StudentDatabase.php
@@ -1,25 +1,20 @@
 <?php
+	include_once("Student.php");
+	include_once("DatabaseConnector.php");
     
-    include_once("Student.php");
-    include_once("DatabaseConnector.php");
-    
-    final class StudentDatabase
-    {
-
-    	static public function insertStudent($student)
-        {
-            $values = "('"	. $student->getEmail() . "','"
-            				. $student->getFirstName() . "','"
-            				. $student->getSurName() . "')";
-
-    
-            $sql =  "INSERT INTO Students (Email, FirstName, Surname) 
-                 	 VALUES $values";
-    
-            return DatabaseConnector::getConnection()->query($sql) ? true : false;
-        } 
-        
-
-        private function __construct() { }
-    }
+	final class StudentDatabase
+	{
+		static public function insertStudent($student)
+		{
+			$values = "('"	. $student->getEmail() . "','"
+							. $student->getFirstName() . "','"
+							. $student->getSurName() . "')";
+			
+			$sql =  "INSERT INTO Students (Email, FirstName, Surname) VALUES $values";
+			
+			return DatabaseConnector::getConnection()->query($sql) ? true : false;
+		} 
+		
+		private function __construct() { }
+	}
 ?>
