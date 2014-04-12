f786e99d410eed3a180fbc6175173ca215322fe3
Konrad Welc <konrad1331@gmail.com>
2014-04-12 15:02:16 +0200

    Wstawiam usunięty studentDatabase ...
    Serio przestańcie się bawić klasami bazodanowymi, a tymbardziej
    przestańcie komitować kod na ślepo, srawdzajcie co najpierw commitujecie
    i jakie zmiany zostają wprowadzone !!!

--- lib/Database/StudentDatabase.php
+++ lib/Database/StudentDatabase.php
@@ -3,23 +3,64 @@
 	include_once("Student.php");
 	include_once("DatabaseConnector.php");
 	
-	final class StudentDatabase
-	{
-
+ 	final class StudentDatabase
+ 	{
+	
+		// Do testów
+		static public function getStudentID($student)
+		{ 
+			$sql = "Select * from Students WHERE Email  = '" . $student->getEmail() . "'";
+			$result = DatabaseConnector::getConnection()->query($sql);
+		
+			$row = $result->fetch_array(MYSQLI_NUM);
+			return $row[0];
+		}
+	
+	
 		static public function insertStudent($student)
 		{
 			$values = "('"	. $student->getEmail() . "','"
-							. $student->getFirstName() . "','"
-							. $student->getSurName() . "')";
-
-	
-			$sql =  "INSERT INTO Students (Email, FirstName, Surname) 
-					 VALUES $values";
-	
+			                . strval(md5(microtime())) . "','"
+			                . $student->getFirstName() . "','"
+			                . $student->getSurName() . "')";
+			
+			$sql =  "INSERT INTO Students (Email, Code, FirstName, Surname) VALUES $values";
 			return DatabaseConnector::getConnection()->query($sql) ? true : false;
 		} 
 		
-
+		static public function updateStudent($student)
+		{
+			$sql = "Select * from Students WHERE ID  = '" . $student->getID() . "'";
+			$result = DatabaseConnector::getConnection()->query($sql);
+			if ($result->num_rows == 0) { 
+				return false;
+			}
+		
+			$sql = "UPDATE Students SET 
+			        Email  = '" . $student->getEmail() . "', 
+			        FirstName = '" . $student->getFirstName() . "', 
+			        Surname = '" . $student->getSurName() . "'
+			        WHERE ID = '" . $student->getID() . "'";
+			
+			return DatabaseConnector::getConnection()->query($sql) ? true : false;
+		} 
+    
+		/*
+		 * Usunięcie egzaminu z bazy danych, wraz ze sprawdzeniem czy dany egzaminator
+		 * zamieścił egzamin i ma do tego uprawnienia
+		 */ 
+		static public function deleteStudent($student)
+		{
+			$sql = "Select * from Students WHERE ID  = '" . $student->getID() . "'";
+			$result = DatabaseConnector::getConnection()->query($sql);
+			if ($result->num_rows == 0) { 
+				return false;
+			}
+		
+			$sql = "Delete from Students WHERE ID  = '" . $student->getID() . "'";
+			return DatabaseConnector::getConnection()->query($sql) ? true : false;
+		}
+		
 		private function __construct() { }
 	}
 ?>
