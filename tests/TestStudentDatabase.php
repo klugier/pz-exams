ff89286ee9f440797b03d0b3ac5fdc7008e8d040
Konrad Welc <konrad1331@gmail.com>
2014-04-12 02:41:35 +0200

    StudentDatabase usuniecie inserta tablicowego

--- tests/TestStudentDatabase.php
+++ tests/TestStudentDatabase.php
@@ -3,21 +3,10 @@
 	
 	include_once('../lib/Lib.php');
 	
-	$testStudents[0] = new Student;
-	$testStudents[1] = new Student;
-	$testStudents[2] = new Student;
-	
+	$testStudents[0] = new Student;	
 	$testStudents[0]->setEmail('test1@test.pl');
 	$testStudents[0]->setFirstName('a');
 	$testStudents[0]->setSurName('aa');
-	
-	$testStudents[1]->setEmail('test2@test.pl');
-	$testStudents[1]->setFirstName('b');
-	$testStudents[1]->setSurName('bb');
-	
-	$testStudents[2]->setEmail('test3@test.pl');
-	$testStudents[2]->setFirstName('c');
-	$testStudents[2]->setSurName('cc');
 	
 	if(StudentDatabase::insertStudent($testStudents[0])){
 		echo "Done and Done <br/>";
@@ -25,8 +14,7 @@
 		echo "nope <br/>";
 	}
 	
-	$testStudents[0]->setID(StudentDatabase::getStudentID($testStudents[0]));
-	
+	$testStudents[0]->setID(StudentDatabase::getStudentID($testStudents[0]));	
 	$testStudents[0]->setFirstName('k');
 	
 	if(StudentDatabase::updateStudent($testStudents[0])){
@@ -47,15 +35,6 @@
 	}else{
 		echo "nope <br/>";
 	}
-	
-	$testStudents[0]->setID(StudentDatabase::getStudentID($testStudents[0]));
-	$testStudents[1]->setID(StudentDatabase::getStudentID($testStudents[1]));
-	$testStudents[2]->setID(StudentDatabase::getStudentID($testStudents[2]));
-	
-	StudentDatabase::deleteStudent($testStudents[0]);
-	StudentDatabase::deleteStudent($testStudents[1]);
-	StudentDatabase::deleteStudent($testStudents[2]);
-	
 	
 	include("TestEnd.php");
 ?>
