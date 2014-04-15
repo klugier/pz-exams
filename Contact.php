70892e22d4a92cc753291c800f8b69811830d7bc
studentuj <michal.gawryluk@uj.edu.pl>
2014-04-15 17:03:11 +0200

    formatowanie zamiast spacji tabulacje, wydzielono skrypt js do odzielnego pliku

--- Contact.php
+++ Contact.php
@@ -1,125 +1,77 @@
+
+
 <?php
-   include_once("lib/Lib.php");
-   
-   $title = "$appName - Kontakt";
-   include("html/Begin.php");
-?>
-
+	include_once("lib/Lib.php");   
+	$title = "$appName - Kontakt";
+	$scriptsDefer = array("js/ValidateContactForm.js");
+	include("html/Begin.php");
+	?>
 <form id="contactForm" class="form-horizontal" action="./php/HandlingContactForm.php" method="post">
-
-  <div class="form-group">
-    <fieldset>
-      <legend>Formularz kontaktowy</legend>
-    </fieldset>
-  </div>
-  
-  <div class="form-group">
-    <div class="control-group">
-      <label class="col-xs-2 col-sm-2 col-md-2  control-label" for="inputEmail">Email</label>
-      <div class="col-xs-4 col-sm-4 col-md-4 controls">
-	<input class="form-control" type="text" name="email" id="email" placeholder="Email">
-      </div>
-      <span class="help-block col-xs-6 col-sm-6 col-md-6" id="email-error-message" >		      
-      </span>
-    </div>
-  </div>
-  <div class="control-group">
-    <div class="form-group">
-      <label class="col-xs-2 col-sm-2 col-md-2  control-label" for="inputSubject">Temat</label>
-      <div class="col-xs-4 col-sm-4 col-md-4 controls">
-	<input class="form-control" type="text" name="subject" id="subject" placeholder="Temat">
-      </div>
-      <span class="help-block col-xs-6 col-sm-6 col-md-6" id="subject-error-message" >
-      </span>
-    </div>
-  </div>
-  <div class="control-group">
-    <div class="form-group">
-      <label class="col-xs-2 col-sm-2 col-md-2  control-label" for="inputMessage">Wiadomość</label>
-      <div class="col-xs-5 col-sm-5 col-md-5 controls">
-	<textarea class="form-control" type="text" name="message" id="message" placeholder="Wiadomość" rows="10"></textarea>	
-      </div>
-      <span class="help-block col-xs-4 col-sm-4 col-md-4" id="message-error-message" >
-      </span>
-    </div>
-  </div>
-  <div class="form-group" >
-    <label class="col-xs-2 col-sm-2 col-md-2 control-label"> Przepisz kod z obrazka </label>
-    <div class="col-xs-4 col-sm-4 col-md-4">
-      <img id="captcha" src="lib/SecureImage/securimage_show.php"  class="img-thumbnail .img-rounded:2px" style="margin-right:20px;" alt="CAPTCHA IMAGE" />
-      <div style="display:inline-block;vertical-align:middle;float:none;">
-	<a href="#" onclick="document.getElementById('captcha').src = 'lib/SecureImage/securimage_show.php?' + Math.random(); return false"> 
-	  <img id="reload-image" src="lib/SecureImage/images/refresh.png" alt="Odśwież Obrazek" />
-	</a> 
-      </div>
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="control-group">
-      <div class="col-xs-offset-2 col-sm-offset-2 col-md-offset-2 col-xs-4 col-sm-4 col-md-4">
-	<input type="text" class="form-control" name="captcha_code" id="captcha_code"> 
-      </div>
-      <span class="help-block col-xs-4 col-sm-4 col-md-4" id="captcha_code-error-message">
-      </span>
-    </div> 
-  </div>
-  <div class="form-group">
-    <div class="control-group">
-      <div class="col-xs-12 col-sm-12 col-md-12 controls">
-	<button type="submit" class="btn btn-lg btn-block btn-primary">Wyślij</button>
-      </div>
-    </div>
-  </div>
+	<div class="form-group">
+		<fieldset>
+			<legend>Formularz kontaktowy</legend>
+		</fieldset>
+	</div>
+	<div class="form-group">
+		<div class="control-group">
+			<label class="col-xs-2 col-sm-2 col-md-2  control-label" for="inputEmail">Email</label>
+			<div class="col-xs-4 col-sm-4 col-md-4 controls">
+				<input class="form-control" type="text" name="email" id="email" placeholder="Email">
+			</div>
+			<span class="help-block col-xs-6 col-sm-6 col-md-6" id="email-error-message" >		      
+			</span>
+		</div>
+	</div>
+	<div class="control-group">
+		<div class="form-group">
+			<label class="col-xs-2 col-sm-2 col-md-2  control-label" for="inputSubject">Temat</label>
+			<div class="col-xs-4 col-sm-4 col-md-4 controls">
+				<input class="form-control" type="text" name="subject" id="subject" placeholder="Temat">
+			</div>
+			<span class="help-block col-xs-6 col-sm-6 col-md-6" id="subject-error-message" >
+			</span>
+		</div>
+	</div>
+	<div class="control-group">
+		<div class="form-group">
+			<label class="col-xs-2 col-sm-2 col-md-2  control-label" for="inputMessage">Wiadomość</label>
+			<div class="col-xs-5 col-sm-5 col-md-5 controls">
+				<textarea class="form-control" type="text" name="message" id="message" placeholder="Wiadomość" rows="10"></textarea>	
+			</div>
+			<span class="help-block col-xs-4 col-sm-4 col-md-4" id="message-error-message" >
+			</span>
+		</div>
+	</div>
+	<div class="form-group" >
+		<label class="col-xs-2 col-sm-2 col-md-2 control-label"> Przepisz kod z obrazka </label>
+		<div class="col-xs-4 col-sm-4 col-md-4">
+			<img id="captcha" src="lib/SecureImage/securimage_show.php"  class="img-thumbnail .img-rounded:2px" style="margin-right:20px;" alt="CAPTCHA IMAGE" />
+			<div style="display:inline-block;vertical-align:middle;float:none;">
+				<a href="#" onclick="document.getElementById('captcha').src = 'lib/SecureImage/securimage_show.php?' + Math.random(); return false"> 
+				<img id="reload-image" src="lib/SecureImage/images/refresh.png" alt="Odśwież Obrazek" />
+				</a> 
+			</div>
+		</div>
+	</div>
+	<div class="form-group">
+		<div class="control-group">
+			<div class="col-xs-offset-2 col-sm-offset-2 col-md-offset-2 col-xs-4 col-sm-4 col-md-4">
+				<input type="text" class="form-control" name="captcha_code" id="captcha_code"> 
+			</div>
+			<span class="help-block col-xs-4 col-sm-4 col-md-4" id="captcha_code-error-message">
+			</span>
+		</div>
+	</div>
+	<div class="form-group">
+		<div class="control-group">
+			<div class="col-xs-12 col-sm-12 col-md-12 controls">
+				<button type="submit" class="btn btn-lg btn-block btn-primary">Wyślij</button>
+			</div>
+		</div>
+	</div>
 </form>
-
 <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.js"></script>
 <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
-
-<script>
-$("#contactForm").validate({
-    errorElement: 'span',
-    rules: {
-	subject: "required",
-	message: "required",
-	captcha_code: {
-	    required: true,
-	    minlength: 6
-	},
-	email: {
-	    required: true,
-	    email: true
-	}
-    },
-    messages: {
-	subject: "Prosz podac temat",
-	message: "Prosze wpisac tresc wiadomosci",
-	captcha_code: {
-	    required: "Prosze wpisac kod z obrazka",
-	    minlength: "Wpisano nieprawidlowa ilosc znakow"
-	},
-	email: {
-	    required: "Prosze podac swoj kontaktowy adres e-mail",
-	    email: "Adres powinien posiadac format name@domain.com"
-	}
-    },
-    //odpowiedzialne za umieszcenie komunikatu wewntrz odpowiedniego element
-    //o id [element]-error-message, gdzie [element] jest podany jako parametr funkcji
-    errorPlacement: function(error, element) {
-        var name = $(element).attr("name");
-	if(error.length>0){
-	    $("#" + name + "-error-message").append("<span class=\"badge pull-left\" style=\"background-color:#F13333;\">!</span>");
-	    error.appendTo($("#" + name + "-error-message"));
-	    $("#" + name + "-error-message").children("span").eq(1).css(
-		{
-		    padding: '5px',
-		    color: '#B94A48'
-		}
-	    );
-	}else{
-	    $("#" + name + "-error-message").empty();
-	}
-    }
-});
-</script>
-
 <?php include ("html/End.php"); ?>
+
+
--- js/ValidateContactForm.js
+++ js/ValidateContactForm.js
@@ -1,120 +1,43 @@
-jQuery( document ).ready(function( $ ) {
-	// enums error service 
-	var emailFieldErrorType =  { 
-		BAD_DOMAIN : 0 , 
-		NOT_AN_EMAIL : 1 , 
-		ADRESS_ALREADY_USED : 2 
-	} ;  
-	
-	var captchaFieldErrorType = { 
-		LENGTH_IMPROPER : 0 
-	} ; 
-	// functions error service  
-	function deleteSelectorError ( $selector )
-	{ 
-			$($selector).html("");
-	} 
-	
-	function emailError ( $errorCode ) 
-	{ 
-		if  ($errorCode ==  emailFieldErrorType.NOT_AN_EMAIL) {
-			$("#email-error-message").html('<span style="background-color:#F13333;" class="badge pull-left ">!</span>' +
-											'<span style="padding:5px">Podany adress nie jest emailem.</span>') ; 
-			return ; 
-		} 
-		if  ($errorCode ==  emailFieldErrorType.BAD_DOMAIN) { 
-			$("#email-error-message").html('<span style="background-color:#F13333;" class="badge pull-left ">!</span>' +
-											'<span style="padding:5px"> Przepraszamy w chwiki opecnej ta domena nie jest obsługiwana.</span>') ; 
-			return ; 
-		} 
-		if  ($errorCode ==  emailFieldErrorType.ADRESS_ALREADY_USED) { 
-			$("#email-error-message").html('<span style="background-color:#F13333;" class="badge pull-left ">!</span>' +
-											'<span style="padding:5px">Na podany adres zarejestrowano już konto.</span>') ; 
-			return ; 
+$("#contactForm").validate({
+	errorElement: 'span',
+	rules: {
+		subject: "required",
+		message: "required",
+		captcha_code: {
+		required: true,
+		minlength: 6
+		},
+		email: {
+			required: true,
+			email: true
 		}
-	} 
-	
-	function captchaError ( $errorCode )
-	{ 
-		if ( $errorCode == captchaFieldErrorType.LENGTH_IMPROPER  ) { 
-			$("#captcha-error-message").html('<span style="background-color:#F13333;" class="badge pull-left">!</span>' + 
-											  '<span style="padding:5px">Długość kodu niepoprawna</span>') ; 
-			return ; 
-		} 
-		
-	} 
-	
-
-	
-	// jQuery events 
-	
-	$("#register_form").submit(function(e){
-		var regex_pattern =  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		if (!regex_pattern.test($("#email").val())
-			|| ($("#email").val().indexOf("uj.edu.pl") == -1)
-		) {
-			//alert ("can't  submit email "); 
-			return false ;
-		} 
-
-		if ( $("#captcha_code").val().length != 6 ) { 
-			$("#captcha-error-message").css('visibility' , 'visible');	
-			return false ; 
-		} 
-		return true ; 
-	}) ; 
-	
-	
-	
-	$("#email").focusout( function ( event){
-		var email = $(this).val();
-		var $formGroup = $(this).closest('div[class^="form-group"]') ;
-		if ( email.length == 0 ) {  
-			// alert("email length ");
-			$formGroup.removeClass('has-error');
-			$formGroup.removeClass('has-success');
-			deleteSelectorError ( "#email-error-message" );
-			return ; 
-		} 
-		var emailRegexPattern =  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		 
-		// alert ( checkIfUserInDatabase(email)   ) ;  
-		
-		if (!emailRegexPattern.test(email)) { 
-			// alert() ;
-			emailError ( emailFieldErrorType.NOT_AN_EMAIL  ) ;
-			$formGroup.removeClass('has-success');
-			$formGroup.addClass('has-error');
-		} 
-		else 
-		{ 
-			//alert($(this).val()) ;
-			deleteSelectorError ( "#email-error-message" ); 
-			$formGroup.removeClass('has-error');
-			$formGroup.addClass('has-success');
+	},
+	messages: {
+		subject: "Prosz podac temat",
+		message: "Prosze wpisac tresc wiadomosci",
+		captcha_code: {
+			required: "Prosze wpisac kod z obrazka",
+			minlength: "Wpisano nieprawidlowa ilosc znakow"
+		},
+		email: {
+			required: "Prosze podac swoj kontaktowy adres e-mail",
+			email: "Adres powinien posiadac format name@domain.com"
 		}
-	} ); 
-
-	
-	
-	$("#captcha_code").focusout( function (event) {
-		if ( $(this).val().length != 6 &&  $(this).val().length != 0   )  { 
-			captchaError ( captchaFieldErrorType.LENGTH_IMPROPER  ) ; 
-		} else { 
-			deleteSelectorError( "#captcha-error-message");
-		} 
-	} ) ; 
-	
-	$( "div#invalid-captcha-code" ).delay(5000).slideUp("slow");
-	
-	/*$("#passwd-repeat").keyup( function (event) { 
-		var repeatedPasswd = $(this).val();
-		var passwd = $('input[id="passwd"]').val(); 
-		var $formGroup = $(this).closest('div[class^="form-group"]') ;
-		// alert ( passwd +" -- "+ repeatedPasswd ) ; // only testing purposes 
-		if ( repeatedPasswd != passwd ) 
-		{ 
-			$formGroup.addClass('has-warning'); 
-		}    
-	} ) ; */
-}); 
+	},
+	//odpowiedzialne za umieszcenie komunikatu wewntrz odpowiedniego element
+	//o id [element]-error-message, gdzie [element] jest podany jako parametr funkcji
+	errorPlacement: function(error, element) {
+		var name = $(element).attr("name");
+		if(error.length>0){
+			$("#" + name + "-error-message").append("<span class=\"badge pull-left\" style=\"background-color:#F13333;\">!</span>");
+			error.appendTo($("#" + name + "-error-message"));
+			$("#" + name + "-error-message").children("span").eq(1).css(
+			{
+				padding: '5px',
+				color: '#B94A48'
+			});
+		}else{
+			$("#" + name + "-error-message").empty();
+		}
+	}
+});
