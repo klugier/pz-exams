dccb8898e8acef2d91bb708cf3d0da4e9c9314cf
Arkadiusz Koszczan <arkadiusz.koszczan@gmail.com>
2014-04-21 19:56:34 +0200

    zmiana wyglądu podstrony edycja egzaminu

--- ExamEdit.php
+++ ExamEdit.php
@@ -14,17 +14,39 @@
 		ob_end_flush();
 		return;
 	}
-	
-	include("html/UserPanel.php");
-	
 ?>
 
+<div class="container col-md-12">
+	<h2>Edycja Egzaminu</h2>
+	<hr/>
+	<form role="form" class="form-horizontal" >
 
-<h2> Edycja Egzaminu </h2>
-<div id="stages">
-	<?php
-		include("html/ExamStage1.php");
-	?>
+		<div class="form-group" id="exam_name_group">
+			<label for="exam_name" class="control-label col-sm-3 col-md-3">Nazwa egzaminu</label>
+			<div class="col-sm-4 col-md-4">
+				<input type="text" class="form-control" id="exam_name" placeholder="Zmień nazwę" maxlength="120" >
+			</div>
+		</div>
+
+
+		<div class="form-group" id="duration_group">
+			<label for="duration" class="control-label col-sm-3 col-md-3">Czas trwania egzaminu (minuty)</label>
+
+			<div class="col-sm-4 col-md-4">
+				<input type="number" name="duration" class="form-control" id="exam_duration" placeholder="Zmień czas trwania" maxlength="2" min="0" max="100">
+			</div>
+		</div>
+	</form>
+	<div class="row col-md-11" style="float:none;margin:0 auto;"> 
+			<br />
+				<div id="calendar-control"> 
+				<!-- here goes calendar content --> 
+				</div>
+			<br />
+			<div class="pull-right">
+				<button type="button" class="btn btn-primary" id="exam-list-return" style="padding-left: 30px; padding-right: 30px;">Lista Egzaminów</button>
+			</div>
+	</div>
 </div>
 
 <script type="text/javascript">
