13cc54aa909e4f0109c791a2f6b59aba0a778039
studentuj <michal.gawryluk@uj.edu.pl>
2014-04-23 16:06:53 +0200

    Poprawiono bledy w pliku HandlingContactForm.php

--- php/HandlingContactForm.php
+++ php/HandlingContactForm.php
@@ -1,8 +1,9 @@
 <?php
+echo 'to wysylanie';
 
 require_once '../lib/SecureImage/securimage.php';
-include_once("../lib/Lib.php");
-require_once '../lib/Utility/Mail.php');
+include_once "../lib/Lib.php";
+require_once '../lib/Utility/Mail.php';
 
 $captcha_code        = $_POST['captcha_code'];
 
@@ -10,37 +11,39 @@ $securimage = new Securimage();
 
 if ($securimage->check($captcha_code) == true) { 
   
-  if (empty($_POST['email'])) {
-    //puste pole email
-  } else { 
-    //zapisz email
-    $cMail=$_POST['email'];
-  }
+	if (empty($_POST['email'])) {
+		//puste pole email
+		echo 'brak maila';
+	} else { 
+		//zapisz email
+		$cMail=$_POST['email'];
+	}
   
-  if (empty($_POST['subject'])){
-    //pusty temat
-  } else {
-    //zapisz temat
-    $cSubject=$_POST['subject'];
-  }
+	if (empty($_POST['subject'])){
+		//pusty temat
+		echo 'brak tematu';
+	} else {
+		//zapisz temat
+		$cSubject=$_POST['subject'];
+	}
   
-  if (empty($_POST['message'])){
-    //pusta wiadomosc
-  } else {
-    //zapisz wiadomosc
-    $cMessage=$_PSOT['message'];
-  }
+	if (empty($_POST['message'])){
+		//pusta wiadomosc
+		echo 'brak wiadomosc';
+	} else {
+		//zapisz wiadomosc
+		$cMessage=$_PSOT['message'];
+	}
   
-  echo 'captcha code valid'; 
+	echo 'captcha code valid'; 
   
-  //cMail - adres podany w formularzy
-  //cSubject - temat podany w formularzu
-  //cMessage - tresc komunikatu
-  mailer('GUSER',$cMail,'Formularz kontaktowy',$cSubject,$cMessage,false);
- } else {
-  $_SESSION['formErrorCode'] = 'invalidCaptcha';
-  header('Location: ../Contact.php' ); 
- }
+	//cMail - adres podany w formularzy
+	//cSubject - temat podany w formularzu
+	//cMessage - tresc komunikatu
+	if(mailer('GUSER',$cMail,'Formularz kontaktowy',$cSubject,$cMessage,false))
+		echo 'Funkcja mailer() zwrocila prawde';
+} else {
+	$_SESSION['formErrorCode'] = 'invalidCaptcha';
+	header('Location: ../Contact.php' ); 
 }
-?>
-
+;?>
