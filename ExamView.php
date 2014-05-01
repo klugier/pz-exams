f472d870379801a5933af1f143d0777bc2db0138
Michał P <p.michal1990@gmail.com>
2014-05-01 12:53:15 +0200

    wprowadzono podstawowy widok i zabezpieczenia

--- ExamView.php
+++ ExamView.php
@@ -3,6 +3,30 @@
 
 	$title = "$appName - Widok egzaminu";
 	include("html/Begin.php");
+
+	if (!isset($_SESSION['USER']) || $_SESSION['USER'] == "") {
+		echo "<div class=\"alert alert-danger\"><b>Strona widoczna jedynie dla zalogowanych użytkowników.</b> Za 3 sekundy zostaniesz przeniesiony na stronę główną.</div>";
+		header("refresh: 3; url=index.php");
+		include("html/End.php");
+		
+		ob_end_flush();
+		return;
+	}
+
+	$userExamList = ExamDatabase::getExamList(unserialize($_SESSION['USER'])->getID());
+	$accessEditExamGranted = false ; 
+	foreach ($userExamList as $exam) { 
+		if ( $exam->getID() == $_GET['id'] ) $accessEditExamGranted = true; 
+	} 
+	if ( ! $accessEditExamGranted ) header('Location: ExamList.php') ;	
+	
+	
+	include("html/UserPanel.php");
+
+	$id   = $_GET['id'];
+	$exam = ExamDatabase::getExam($id);
+	echo "<h2>Informacje o egzaminie</h2>";
+	echo "<h4><i>(" . $exam->getName() . ")</i></h4>";
 ?>
 
 <?php
