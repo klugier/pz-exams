a155bf33bde03501585d33369d5e3f0f283fd29f
Zbigniew Rębacz <klugier@poczta.onet.pl>
2014-05-02 20:05:20 +0200

    Dostosowanie widoku egzaminu do pozostałych podstron

--- ExamView.php
+++ ExamView.php
@@ -13,22 +13,29 @@
 		return;
 	}
 	
+	include("html/UserPanel.php");
+	
+	if (!isset($_GET['id'])) {
+		include("html/End.php");
+		return;
+	}
+	
 	$userExamList = ExamDatabase::getExamList(unserialize($_SESSION['USER'])->getID());
-	$accessEditExamGranted = false ; 
+	$accessEditExamGranted = false; 
 	foreach ($userExamList as $exam) { 
 		if ( $exam->getID() == $_GET['id'] ) $accessEditExamGranted = true; 
 	} 
-	if ( ! $accessEditExamGranted ) header('Location: ExamList.php') ;	
-	
-	
-	include("html/UserPanel.php");
+	if (!$accessEditExamGranted)
+		header('Location: ExamList.php');	
 	
 	$id   = $_GET['id'];
 	$exam = ExamDatabase::getExam($id);
-	echo "<h2>Informacje o egzaminie</h2>";
-	echo "<a class=\"btn btn-primary btn-sm pull-right\" href=\"php/PDFExamRegisteredStudentsList.php?examID=".$exam->getID()."\" role=\"button\" name=\"examRegisteredStudentsListPDFGlyph\" id=\"examRegisteredStudentsListPDFGlyph\" title=\"Pobierz PDF\" value=\"".$exam->getID()."\"\><i class=\"glyphicon glyphicon-download\"></i> <b>PDF</b></a>";
+	echo "<h2>";
+	echo "<span>Informacje o egzaminie</span>";
+	echo "<span style=\"float: right\"><a class=\"btn btn-primary btn-sm pull-right\" href=\"php/PDFExamRegisteredStudentsList.php?examID=".$exam->getID()."\" role=\"button\" name=\"examRegisteredStudentsListPDFGlyph\" id=\"examRegisteredStudentsListPDFGlyph\" title=\"Pobierz PDF\" value=\"".$exam->getID()."\"><i class=\"glyphicon glyphicon-download\"></i> <b>PDF</b></a></span>";
+	echo "</h2>";
 	echo "<h4><i>(" . $exam->getName() . ")</i></h4><hr>";
-
+	
 	$examDays = ExamUnitDatabase::getExamDays($id);
 	$uniqeDays = array_unique($examDays);
 	$weekDays = array(1 => "Poniedziałek", 2 => "Wtorek", 3 => "Środa", 4 => "Czwartek", 5 => "Piątek", 6 => "Sobota", 0 => "Niedziela");
@@ -36,7 +43,7 @@
 	foreach ($uniqeDays as $day) {
 	echo "<h4 class=\"bg-info\">".$day." (".$weekDays[strftime('%w',strtotime($day))].")</h4>";
 	echo '
-		<table class="table table-hover table-condensed text-left">
+		<table class="table table-condensed text-left">
 			<thead>
 				<tr class="row">
 					<th class="col-md-1"><center>Lp.</center></th>
@@ -53,7 +60,7 @@
 
 	foreach ($examUnitIDList as $examUnitID) {
 		echo "<tr class=\"row\">";
-		echo "<td class=\"text-center col-md-1\" style=\"vertical-align:middle;\">" . $i . "</td>\n";
+		echo "<td class=\"text-center col-md-1\" style=\"vertical-align:middle;\">" . $i . ".</td>\n";
 
 		$examunit = ExamUnitDatabase::getExamUnit($examUnitID);
 		echo "<td class=\"col-md-2\" style=\"vertical-align:middle;\">" . $examunit->getTimeFrom()." - ". $examunit->getTimeTo() . "</td>\n";
@@ -61,11 +68,11 @@
 		$record = RecordDatabase::getRecordFromUnit($examUnitID);
 		if($record == NULL){
 			echo "<td class=\"col-md-9\" style=\"vertical-align:middle;\"> ----- </td>\n";
-		}else{
+		} else {
 			$student = StudentDatabase::getStudentByID($record->getStudentID());
 			if(($student->getFirstName() == NULL) || ($student->getFirstName() == "")){
 				echo "<td class=\"col-md-9\" style=\"vertical-align:middle;\">" . $student->getEmail() . "</td>\n";
-			}else{
+			} else {
 				echo "<td class=\"col-md-9\" style=\"vertical-align:middle;\">" . $student->getFirstName() . " " . $student->getSurName() . "</td>\n";
 			}
 			$student = NULL;
--- php/PDFExamRegisteredStudentsList.php
+++ php/PDFExamRegisteredStudentsList.php
@@ -2,40 +2,40 @@
 	include_once("../lib/Lib.php");
 	$id   = $_GET['examID'];
 	$exam = ExamDatabase::getExam($id);
+	
 	class PDF extends TFPDF
+	{
+		function Table($header, $id)
 		{
-	
-			function Table($header, $id)
-			{//
-				// Column widths
-				$w = array(10, 50, 50, 50);
-	
-				$examDays = ExamUnitDatabase::getExamDays($id);
-				$uniqeDays = array_unique($examDays);
-				$weekDays = array(1 => "Poniedziałek", 2 => "Wtorek", 3 => "Środa", 4 => "Czwartek", 5 => "Piątek", 6 => "Sobota", 0 => "Niedziela");
-	
-				foreach ($uniqeDays as $dayCount => $day) {
+			// Column widths
+			$w = array(10, 50, 50, 50);
+			
+			$examDays = ExamUnitDatabase::getExamDays($id);
+			$uniqeDays = array_unique($examDays);
+			$weekDays = array(1 => "Poniedziałek", 2 => "Wtorek", 3 => "Środa", 4 => "Czwartek", 5 => "Piątek", 6 => "Sobota", 0 => "Niedziela");
+			
+			foreach ($uniqeDays as $dayCount => $day) {
 				$this->Cell(160,10,$day." (".$weekDays[strftime('%w',strtotime($day))].")",0,0,'L');
 				$this->Ln();
 				// Header
-				for($i=0;$i<count($header);$i++)
-					$this->Cell($w[$i],7,$header[$i],1,0,'C');
+				for ($i=0; $i < count($header); $i++)
+					$this->Cell($w[$i], 7, $header[$i], 1, 0, 'C');
 				$this->Ln();
 				// Data
 				$examUnitIDList = ExamUnitDatabase::getExamUnitIDListDay($id, $day);
-	
+			
 				foreach ($examUnitIDList as $number =>  $examUnitID) {
-					$this->Cell($w[0],6,number_format($number+1),'LR',0,'C');
+					$this->Cell($w[0], 6, number_format($number+1) . ".", 'LR', 0, 'C');
 					$examunit = ExamUnitDatabase::getExamUnit($examUnitID);
-					$this->Cell($w[1],6,$examunit->getTimeFrom()." - ". $examunit->getTimeTo(),'LR',0,'C');
+					$this->Cell($w[1], 6, $examunit->getTimeFrom()." - ". $examunit->getTimeTo(), 'LR', 0, 'C');
 					$record = RecordDatabase::getRecordFromUnit($examUnitID);
 					if($record == NULL){
 						$this->Cell($w[2],6,"",'LR',0,'C');
-					}else{
+					} else {
 						$student = StudentDatabase::getStudentByID($record->getStudentID());			
-						if(($student->getFirstName() == NULL) || ($student->getFirstName() == "")){
+						if (($student->getFirstName() == NULL) || ($student->getFirstName() == "")){
 							$this->Cell($w[2],6,$student->getEmail(),'LR',0,'C');
-						}else{
+						} else {
 							$this->Cell($w[2],6,$student->getFirstName() . " " . $student->getSurName(),'LR',0,'C');
 						}
 						$student = NULL;
@@ -44,25 +44,24 @@
 					$this->Ln();
 				}
 				// Closing line
-				$this->Cell(array_sum($w),0,'','T');
-				if($dayCount != count($uniqeDays)-1){
+				$this->Cell(array_sum($w), 0, '', 'T');
+				if($dayCount != count($uniqeDays)-1) {
 					$this->AddPage();
 				}
-				}
 			}
+		}
 	}
 	
+	$pdf = new PDF();
+	// Column headings
+	$header = array('Lp.', 'Termin', 'Student', 'Podpis');
+	$pdf->AddFont('DejaVu', '', 'DejaVuSansCondensed.ttf', true);
+	$pdf->SetFont('DejaVu', '', 14);
+	$pdf->AddPage();
+	$pdf->Cell(160, 10, $exam->getName(), 0, 0, 'C');
+	$pdf->Ln();
+	$pdf->Table($header, $id);
+	$pdf->Output($exam->getName().".pdf","D");
 	
-		$pdf = new PDF();
-		// Column headings
-		$header = array('Lp.', 'Termin', 'Student', 'Podpis');
-		$pdf->AddFont('DejaVu','','DejaVuSansCondensed.ttf',true);
-		$pdf->SetFont('DejaVu','',14);
-		$pdf->AddPage();
-		$pdf->Cell(160,10,$exam->getName(),0,0,'C');
-		$pdf->Ln();
-		$pdf->Table($header,$id);
-		$pdf->Output($exam->getName().".pdf","D");
-	
-		header('Location: ../ExamView.php?id='.$id); 
+	header('Location: ../ExamView.php?id='.$id); 
 ?>
