46cdf3c5707b97902208f5c56e1c605dfcc39a2b
Michal Szura <michal.szura@gmail.com>
2014-05-08 01:59:31 +0200

    PDF MK 3

--- php/PDFExamRegisteredStudentsList.php
+++ php/PDFExamRegisteredStudentsList.php
@@ -1,206 +1,107 @@
 <?php
 	include_once("../lib/Lib.php");
+	$timeo_start = microtime(true);
+	ini_set("memory_limit","128M");
 	$id = $_GET['examID'];
 	$setting = $_GET['setting'];
 	$exam = ExamDatabase::getExam($id);
 	
-	// extend TCPF with custom functions
-	class MYPDF extends TCPDF {
-	
-		// Colored table
-		public function Table($header, $id, $setting){
-			$w = array(10, 50, 50, 50);
-			if($setting == "full") {
-				// Column widths
-	
-				$examDays = ExamUnitDatabase::getExamDays($id);
-				$uniqeDays = array_unique($examDays);
-				$weekDays = array(1 => "Poniedziałek", 2 => "Wtorek", 3 => "Środa", 4 => "Czwartek", 5 => "Piątek", 6 => "Sobota", 0 => "Niedziela");
-	
-				foreach ($uniqeDays as $dayCount => $day) {
-					$this->Cell(160,10,$day." (".$weekDays[strftime('%w',strtotime($day))].")",0,0,'L');
-					$this->Ln();
-					// Colors, line width and bold font
-					$this->SetFillColor(0, 0, 0);
-					$this->SetTextColor(255);
-					$this->SetLineWidth(0.3);
-					$this->SetFont('', 'B');
-					// Header
-					$w = array(10, 50, 50, 50);
-					$num_headers = count($header);
-					for($i = 0; $i < $num_headers; ++$i) {
-						$this->Cell($w[$i], 7, $header[$i], 1, 0, 'C', 1);
+	$examDays = ExamUnitDatabase::getExamDays($id);
+	$uniqeDays = array_unique($examDays);
+	$weekDays = array(1 => "Poniedziałek", 2 => "Wtorek", 3 => "Środa", 4 => "Czwartek", 5 => "Piątek", 6 => "Sobota", 0 => "Niedziela");
+
+	if($setting == "full") {
+		$html = '<h2>'.$exam->getName().'</h2>';
+		foreach ($uniqeDays as $dayCount => $day) {
+			$html = $html.'<h4>'.$day.' ('.$weekDays[strftime('%w',strtotime($day))].')'.'</h4>';
+			$html = $html.'
+			<table style="width:100%;" border="1">
+			<thead><tr>
+			<th>Lp.</th><th>Termin</th><th>Student</th><th>Podpis</th>
+			</tr></thead><tbody>';
+			$examUnitIDList = ExamUnitDatabase::getExamUnitIDListDay($id, $day);
+			foreach ($examUnitIDList as $number =>  $examUnitID) {
+				$examunit = ExamUnitDatabase::getExamUnit($examUnitID);
+				$record = RecordDatabase::getRecordFromUnit($examUnitID);
+				$html=$html.'<tr height:15px;><td style="width:5%;">'.number_format($number+1).'</td>';
+				$html=$html.'<td style="width:20%;">'.$examunit->getTimeFrom().' - '. $examunit->getTimeTo().'</td>';
+				if($record == NULL) {
+					$html=$html.'<td style="width:25%;"></td>';
+				} else {
+					$student = StudentDatabase::getStudentByID($record->getStudentID());			
+					if (($student->getFirstName() == NULL) || ($student->getFirstName() == "")){
+						$html=$html.'<td style="width:25%;">'.$student->getEmail().'</td>';
+					} else {
+						$html=$html.'<td style="width:25%;">'.$student->getFirstName() .' '. $student->getSurName().'</td>';
 					}
-					$this->Ln();
-					// Color and font restoration
-					$this->SetFillColor(224, 235, 255);
-					$this->SetTextColor(0);
-					$this->SetFont('');
-					// Data
-					$examUnitIDList = ExamUnitDatabase::getExamUnitIDListDay($id, $day);
-					$fill = 0;
-					foreach ($examUnitIDList as $number =>  $examUnitID) {
-						$this->Cell($w[0], 6, number_format($number+1) . ".", 'LR', 0, 'C', $fill);
-						$examunit = ExamUnitDatabase::getExamUnit($examUnitID);
-						$this->Cell($w[1], 6, $examunit->getTimeFrom()." - ". $examunit->getTimeTo(), 'LR', 0, 'C', $fill);
-						$record = RecordDatabase::getRecordFromUnit($examUnitID);
-						if($record == NULL){
-							$this->Cell($w[2],6,"",'LR',0,'C', $fill);
-						} else {
-							$student = StudentDatabase::getStudentByID($record->getStudentID());			
-							if (($student->getFirstName() == NULL) || ($student->getFirstName() == "")){
-								$this->Cell($w[2],6,$student->getEmail(),'LR',0,'C', $fill);
-							} else {
-								$this->Cell($w[2],6,$student->getFirstName() . " " . $student->getSurName(),'LR',0,'C', $fill);
-							}
-							$student = NULL;
-						}
-						$this->Cell($w[3],6,"",'LR',0,'C', $fill);
-						$this->Ln();
-						$fill=!$fill;
-					}
-					// Closing line
-					$this->Cell(array_sum($w), 0, '', 'T');
-					if($dayCount != count($uniqeDays)-1) {
-						$this->AddPage();
-					}
+					$student = NULL;
 				}
-			}elseif($setting == "registered"){
-				// Column widths
-	
-				$examDays = ExamUnitDatabase::getExamDays($id);
-				$uniqeDays = array_unique($examDays);
-				$weekDays = array(1 => "Poniedziałek", 2 => "Wtorek", 3 => "Środa", 4 => "Czwartek", 5 => "Piątek", 6 => "Sobota", 0 => "Niedziela");
-	
-				foreach ($uniqeDays as $dayCount => $day) {
-					$examUnitIDListTest = ExamUnitDatabase::getExamUnitIDListDay($id, $day);
-					$test = TRUE;
-					foreach ($examUnitIDListTest as $examUnitIDTest) {
-						$examunitTest = ExamUnitDatabase::getExamUnit($examUnitIDTest);
-						if($examunitTest->getState() == "locked"){
-							$test = FALSE;
-						}
-					}
-					if($test){
-						continue;
-					}
-					$this->Cell(160,10,$day." (".$weekDays[strftime('%w',strtotime($day))].")",0,0,'L');
-					$this->Ln();
-					// Colors, line width and bold font
-					$this->SetFillColor(0, 0, 0);
-					$this->SetTextColor(255);
-					$this->SetLineWidth(0.3);
-					$this->SetFont('', 'B');
-					// Header
-					$w = array(10, 50, 50, 50);
-					$num_headers = count($header);
-					for($i = 0; $i < $num_headers; ++$i) {
-						$this->Cell($w[$i], 7, $header[$i], 1, 0, 'C', 1);
-					}
-					$this->Ln();
-					// Color and font restoration
-					$this->SetFillColor(224, 235, 255);
-					$this->SetTextColor(0);
-					$this->SetFont('');
-					// Data
-					$examUnitIDList = ExamUnitDatabase::getExamUnitIDListDay($id, $day);
-					$fill = 0;
-					foreach ($examUnitIDList as $number =>  $examUnitID) {
-						$examunit = ExamUnitDatabase::getExamUnit($examUnitID);
-						if($examunit->getState() == "unlocked"){
-							$number = $number-1;
-							continue;
-						}
-						$this->Cell($w[0], 6, number_format($number+1) . ".", 'LR', 0, 'C', $fill);
-						$this->Cell($w[1], 6, $examunit->getTimeFrom()." - ". $examunit->getTimeTo(), 'LR', 0, 'C', $fill);
-						$record = RecordDatabase::getRecordFromUnit($examUnitID);
-						if($record == NULL){
-							$this->Cell($w[2],6,"",'LR',0,'C', $fill);
-						} else {
-							$student = StudentDatabase::getStudentByID($record->getStudentID());			
-							if (($student->getFirstName() == NULL) || ($student->getFirstName() == "")){
-								$this->Cell($w[2],6,$student->getEmail(),'LR',0,'C', $fill);
-							} else {
-								$this->Cell($w[2],6,$student->getFirstName() . " " . $student->getSurName(),'LR',0,'C', $fill);
-							}
-							$student = NULL;
-						}
-						$this->Cell($w[3],6,"",'LR',0,'C', $fill);
-						$this->Ln();
-						$fill=!$fill;
-					}
-					// Closing line
-					$this->Cell(array_sum($w), 0, '', 'T');
-					if($dayCount != count($uniqeDays)-1) {
-						$this->AddPage();
-					}
-				}
-			}else{
-				$this->Cell(160, 10, "Error", 0, 0, 'C');
-				$this->Ln();
+				$html=$html.'<td style="width:50%;"></td></tr>';
+			}
+			$html=$html.'</tbody></table>';
+			if($dayCount != count($uniqeDays)-1) {
+				$html=$html.'<pagebreak />';
 			}
 		}
+	}elseif($setting == "registered"){
+		$html = '<h2>'.$exam->getName().'</h2>';
+		foreach ($uniqeDays as $dayCount => $day) {
+			$examUnitIDListTest = ExamUnitDatabase::getExamUnitIDListDay($id, $day);
+			$test = TRUE;
+			foreach ($examUnitIDListTest as $examUnitIDTest) {
+				$examunitTest = ExamUnitDatabase::getExamUnit($examUnitIDTest);
+				if($examunitTest->getState() == "locked"){
+					$test = FALSE;
+				}
+			}
+			if($test){
+				continue;
+			}
+			$html = $html.'<h4>'.$day.' ('.$weekDays[strftime('%w',strtotime($day))].')'.'</h4>';
+			$html = $html.'
+			<table style="width:100%;" border="1">
+			<thead><tr>
+			<th>Lp.</th><th>Termin</th><th>Student</th><th>Podpis</th>
+			</tr></thead><tbody>';
+			$examUnitIDList = ExamUnitDatabase::getExamUnitIDListDay($id, $day);
+			foreach ($examUnitIDList as $number =>  $examUnitID) {
+				$examunit = ExamUnitDatabase::getExamUnit($examUnitID);
+				if($examunit->getState() == "unlocked"){
+					$number = $number-1;
+					continue;
+				}
+				$record = RecordDatabase::getRecordFromUnit($examUnitID);
+				$html=$html.'<tr height:15px;><td style="width:5%;">'.number_format($number+1).'</td>';
+				$html=$html.'<td style="width:20%;">'.$examunit->getTimeFrom().' - '. $examunit->getTimeTo().'</td>';
+				if($record == NULL) {
+					$html=$html.'<td style="width:25%;"></td>';
+				} else {
+					$student = StudentDatabase::getStudentByID($record->getStudentID());			
+					if (($student->getFirstName() == NULL) || ($student->getFirstName() == "")){
+						$html=$html.'<td style="width:25%;">'.$student->getEmail().'</td>';
+					} else {
+						$html=$html.'<td style="width:25%;">'.$student->getFirstName() .' '. $student->getSurName().'</td>';
+					}
+					$student = NULL;
+				}
+				$html=$html.'<td style="width:50%;"></td></tr>';
+			}
+			$html=$html.'</tbody></table>';
+			if($dayCount != count($uniqeDays)-1) {
+				$html=$html.'<pagebreak />';
+			}
+		}
+	}else{
+		$html = '<h1>ERROR</h1>';
 	}
-	
-	// create new PDF document
-	$pdf = new MYPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
-	
-	// set document information
-	$pdf->SetCreator(PDF_CREATOR);
 
-	// set font
-	$pdf->SetFont('dejavusanscondensed', '', 12);
-
-	// set default header data
-	$user = UserDatabase::getUser($exam->getUserID());
-	$name = $exam->getName();
-	$examiner  = "Egzaminator: ".$user->getFirstName()." ".$user->getSurname();
-	$pdf->SetHeaderData('logo_button.png', 50, $name, $examiner);
+	$mpdf=new mPDF(); 
+	$mpdf->useAdobeCJK = true;
+	$mpdf->SetAutoFont(AUTOFONT_ALL);
+	$stylesheet = file_get_contents('../css/mpdfstyletables.css');
+	$mpdf->WriteHTML($stylesheet,1);
+	$mpdf->WriteHTML($html);
+	$mpdf->Output($exam->getName().".pdf",'D');
 	
-	// set header and footer fonts
-	$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
-	$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
-	
-	// set default monospaced font
-	$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
-	
-	// set margins
-	$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
-	$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
-	$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
-	
-	// set auto page breaks
-	$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
-	
-	// set image scale factor
-	$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
-	
-	// set some language-dependent strings (optional)
-	global $l;
-	$l = Array();
-	$l['a_meta_charset'] = 'UTF-8';
-	$l['a_meta_dir'] = 'ltr';
-	$l['a_meta_language'] = 'pl';-
-	$l['w_page'] = 'strona';
-	$pdf->setLanguageArray($l);
-	
-	// ---------------------------------------------------------
-	
-	// set font
-	//$pdf->SetFont('dejavusanscondensed', '', 12);
-	
-	// add a page
-	$pdf->AddPage();
-	
-	// column titles
-	$header = array('Lp.', 'Imię', 'Nazwisko', 'E-mail');
-	
-	// print table
-	$pdf->Table($header, $id, $setting);
-	
-	// close and output PDF document
-	
-	$pdf->Output($exam->getName().".pdf","D");
-	
-	header('Location: ../ExamStudentsList.php?examID='.$id); 
+	//header('Location: ../ExamStudentsList.php?examID='.$id); 
 ?>
--- php/PDFExamStudentsList.php
+++ php/PDFExamStudentsList.php
@@ -1,113 +1,53 @@
 <?php
-		include_once("../lib/Lib.php");
-		$id   = $_GET['examID'];
-		$exam = ExamDatabase::getExam($id);
+	include_once("../lib/Lib.php");
+	$timeo_start = microtime(true);
+	ini_set("memory_limit","128M");
+	$id   = $_GET['examID'];
+	$exam = ExamDatabase::getExam($id);
 	
-	// extend TCPF with custom functions
-	class MYPDF extends TCPDF {
-	
-		// Colored table
-		public function Table($header,$id) {
-			// Colors, line width and bold font
-			$this->SetFillColor(0, 0, 0);
-			$this->SetTextColor(255);
-			$this->SetLineWidth(0.3);
-			$this->SetFont('', 'B');
-			// Header
-			$w = array(10, 40, 40, 70);
-			$num_headers = count($header);
-			for($i = 0; $i < $num_headers; ++$i) {
-				$this->Cell($w[$i], 7, $header[$i], 1, 0, 'C', 1);
-			}
-			$this->Ln();
-			// Color and font restoration
-			$this->SetFillColor(224, 235, 255);
-			$this->SetTextColor(0);
-			$this->SetFont('');
-			// Data
-			$studentIDList = RecordDatabase::getStudentIDList($id);
-			$studentList = null;
-			if (is_array($studentIDList)) {
-				$i = 0;
-				foreach ($studentIDList as $studentID) {
-					$studentList[$i++] = StudentDatabase::getStudentByID($studentID);
-				}
-			}
-			$fill = 0;
-			foreach($studentList as $number => $student) {
-				$this->Cell($w[0], 6, number_format($number+1), 'LR', 0, 'C', $fill);
-				$this->Cell($w[1], 6, $student->getFirstName(), 'LR', 0, 'C', $fill);
-				$this->Cell($w[2], 6, $student->getSurname()  , 'LR', 0, 'C', $fill);
-				$this->Cell($w[3], 6, $student->getEmail()    , 'LR', 0, 'C', $fill);
-				$this->Ln();
-				$fill=!$fill;
-			}
-			$this->Cell(array_sum($w), 0, '', 'T');
+	$html = '
+	<h2>'.$exam->getName().'</h2>
+	<table style="width:100%;" border="1">
+	<thead><tr>
+	<th>Lp.</th><th>Imię</th><th>Nazwisko</th><th>E-mail</th>
+	</tr></thead><tbody>';
+	$studentIDList = RecordDatabase::getStudentIDList($id);
+	$studentList = null;
+	if (is_array($studentIDList)) {
+		$i = 0;
+		foreach ($studentIDList as $studentID) {
+			$studentList[$i++] = StudentDatabase::getStudentByID($studentID);
 		}
 	}
-	
-	// create new PDF document
-	$pdf = new MYPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
-	
-	// set document information
-	$pdf->SetCreator(PDF_CREATOR);
-	
-	// set default header data
-	$user = UserDatabase::getUser($exam->getUserID());
-	$name = $exam->getName();
-	$examiner  = "Egzaminator: ".$user->getFirstName()." ".$user->getSurname();
-	$pdf->SetHeaderData('logo_button.png', 50, $name, $examiner);
-	
-	// set header and footer fonts
-	$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
-	$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
-	
-	// set default monospaced font
-	$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
-	
-	// set margins
-	$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
-	$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
-	$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
-	
-	// set auto page breaks
-	$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
-	
-	// set image scale factor
-	$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
-	
-	// set some language-dependent strings (optional)
-	global $l;
-	$l = Array();
-	$l['a_meta_charset'] = 'UTF-8';
-	$l['a_meta_dir'] = 'ltr';
-	$l['a_meta_language'] = 'pl';-
-	$l['w_page'] = 'strona';
-	$pdf->setLanguageArray($l);
-	
-	// ---------------------------------------------------------
-	
-	// set font
-	$pdf->SetFont('dejavusanscondensed', '', 12);
-	
-	// add a page
-	$pdf->AddPage();
-	
-	// column titles
-	$header = array('Lp.', 'Imię', 'Nazwisko', 'E-mail');
-	
-	// print table
-	$pdf->Table($header, $id);
-	
-	// ---------------------------------------------------------
-	
-	// close and output PDF document
+	foreach ($studentList as $number => $student) {
+		$html=$html.'<tr><td  style="width:5%;">'.number_format($number+1).'</td>';
+		$html=$html.'<td style="width:20%;">'.$student->getFirstName().'</td>';
+		$html=$html.'<td style="width:20%;">'.$student->getSurname().'</td>';
+		$html=$html.'<td style="width:30%;">'.$student->getEmail().'</td></tr>';
+	}
+	$html=$html.'</tbody></table>';
 
-	$pdf->Output($exam->getName().".pdf","D");
+	$mpdf=new mPDF(); 
+
+	$mpdf->useAdobeCJK = true;
+	$mpdf->SetAutoFont(AUTOFONT_ALL);
+	$stylesheet = file_get_contents('../css/mpdfstyletables.css');
+	$mpdf->WriteHTML($stylesheet,1);
+	$mpdf->WriteHTML($html);
+
+	/*
+	$mpdf=new mPDF('c','A4','','dejavusans',32,25,27,25,16,13); 
+	$mpdf->SetDisplayMode('fullpage');
+	$mpdf->useAdobeCJK = true;
+	$mpdf->SetAutoFont(AUTOFONT_ALL);
+	$mpdf->list_indent_first_level = 0;
+	$mpdf->WriteHTML($html,2);*/
+
+	$mpdf->Output($exam->getName().".pdf",'D');
 	
 	//============================================================+
 	// END OF FILE
 	//============================================================+
 	
-	header('Location: ../ExamStudentsList.php?examID='.$id); 
+	//header('Location: ../ExamStudentsList.php?examID='.$id); 
 ?>
