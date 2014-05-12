ead19bf1310cb0049c1381a02fcdfbbcecef9709
studentuj <michal.gawryluk@uj.edu.pl>
2014-05-12 12:02:58 +0200

    szkielet pomocy - szkic

--- Help.php
+++ Help.php
@@ -20,9 +20,57 @@
 			}
 		?>
 	</ul>
-	
 </div>
-
+<div class="container col-xs-12 col-sm-12 col-md-12">
+	<h4>Tytuł</h4>		
+	<img src="img/help/06_Rejestracja.jpg" alt="" class="col-xs-8 col-sm-8 col-md-8">
+	<p class="col-xs-4 col-sm-4 col-md-4">Treść</p>
+</div>	
+<div class="container col-xs-12 col-sm-12 col-md-12">
+	<h4>Tytuł</h4>		
+	<img src="img/help/00_logowanie.jpg" alt="" class="col-xs-8 col-sm-8 col-md-8">
+	<p class="col-xs-4 col-sm-4 col-md-4">Treść</p>
+</div>
+<div class="container col-xs-12 col-sm-12 col-md-12">
+	<h4>Tytuł</h4>		
+	<img src="img/help/01_po_zalogowaniu.jpg" alt="" class="col-xs-8 col-sm-8 col-md-8">
+	<p class="col-xs-4 col-sm-4 col-md-4">Treść</p>
+</div>	
+<div class="container col-xs-12 col-sm-12 col-md-12">
+	<h4>Tytuł</h4>		
+	<img src="img/help/03_dodaj_egzamin.jpg" alt="" class="col-xs-8 col-sm-8 col-md-8">
+	<p class="col-xs-4 col-sm-4 col-md-4">Treść</p>
+</div>	
+<div class="container col-xs-12 col-sm-12 col-md-12">
+	<h4>Tytuł</h4>		
+	<img src="img/help/03_01_termin.jpg" alt="" class="col-xs-8 col-sm-8 col-md-8">
+	<p class="col-xs-4 col-sm-4 col-md-4">Treść</p>
+</div>	
+<div class="container col-xs-12 col-sm-12 col-md-12">
+	<h4>Tytuł</h4>		
+	<img src="img/help/03_02_lista_studentow.jpg" alt="" class="col-xs-8 col-sm-8 col-md-8">
+	<p class="col-xs-4 col-sm-4 col-md-4">Treść</p>
+</div>	
+<div class="container col-xs-12 col-sm-12 col-md-12">
+	<h4>Tytuł</h4>		
+	<img src="img/help/04_aktualne_egzaminy.jpg" alt="" class="col-xs-8 col-sm-8 col-md-8">
+	<p class="col-xs-4 col-sm-4 col-md-4">Treść</p>
+</div>	
+<div class="container col-xs-12 col-sm-12 col-md-12">
+	<h4>Tytuł</h4>		
+	<img src="img/help/04_01_edytuj_egzamin.jpg" alt="" class="col-xs-8 col-sm-8 col-md-8">
+	<p class="col-xs-4 col-sm-4 col-md-4">Treść</p>
+</div>	
+<div class="container col-xs-12 col-sm-12 col-md-12">
+	<h4>Tytuł</h4>		
+	<img src="img/help/04_02_lista_studentow.jpg" alt="" class="col-xs-8 col-sm-8 col-md-8">
+	<p class="col-xs-4 col-sm-4 col-md-4">Treść</p>
+</div>	
+<div class="container col-xs-12 col-sm-12 col-md-12">
+	<h4>Tytuł</h4>		
+	<img src="img/help/05_archiwalne_egzaminy.jpg" alt="" class="col-xs-8 col-sm-8 col-md-8">
+	<p class="col-xs-4 col-sm-4 col-md-4">Treść</p>
+</div>	
 <?php
 	include ("html/End.php");
 ?>
