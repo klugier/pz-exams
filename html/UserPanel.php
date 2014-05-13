3de76db2bac50a68ca70b717ba1374a7feea7480
Zbigniew Rębacz <klugier@poczta.onet.pl>
2014-05-14 01:07:16 +0200

    Usunięto wpis Start z panelu użytkownika

--- html/UserPanel.php
+++ html/UserPanel.php
@@ -1,5 +1,4 @@
 <div class="navbar navbar-inverse " style="background: rgba(0,0,0,0.75); box-shadow: 2px 2px 20px #444444;">
-	<a class="navbar-brand" href="UserSite.php" id="user_m"><i class="glyphicon glyphicon-home" style="margin-right: 5px;"></i>Start</a>
 	<a class="navbar-brand" href="AddExam.php" id="user_m"><i class="glyphicon glyphicon-plus" style="margin-right: 5px;"></i>Dodaj egzamin</a>
 	<a class="navbar-brand" href="ExamList.php" id="user_m"><i class="glyphicon glyphicon-list" style="margin-right: 5px;"></i>Aktualne egzaminy</a>
 	<a class="navbar-brand" href="ExamListArchives.php" id="user_m"><i class="glyphicon glyphicon-floppy-disk" style="margin-right: 5px;"></i>Archiwalne egzaminy</a>
