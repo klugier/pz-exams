2d0ac2b678166cb1c9ed9cdd9ba7c0612059a72e
Zbigniew Rębacz <klugier@poczta.onet.pl>
2014-05-25 02:32:43 +0200

    .delete zombie code

--- html/End.php
+++ html/End.php
@@ -19,15 +19,6 @@
 
 
 		</div>
-<!-- 		<div id="footer" class="container col-md-8 col-md-offset-2">
-			<p class="pull-left" style="letter-spacing:1px">PZ-Exams 2014. Wszystkie prawa zastrzeżone ©</p>
-			<span class="pull-right" style="margin-top: 1px; margin-left: 80px;">
-				<span><a id="contact" href="?">Kontakt</a></span>
-				<span>| <a id="authors" href="Authors.php">Autorzy</a></span>
-				<span>| <a id="help" href="Help.php">Pomoc</a></span>
-				<span>| <a id="help" href="RegisterForm.php">Rejestracja</a></span>
-			</span>
-		</div> -->
 		
 		<script language="javascript" type="text/javascript" src="js/Lib/bootstrap.min.js"></script>
 		
