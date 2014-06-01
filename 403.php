4b629d150ab3a974d1355efc162b1d1f6036cd02
Zbigniew Rębacz <klugier@poczta.onet.pl>
2014-06-02 01:58:38 +0200

    .cosmetics

--- 403.php
+++ 403.php
@@ -1,24 +1,22 @@
 <?php
 	include_once("lib/Lib.php");
 
-	$title = "$appName - błąd 403 - brak dostępu";
+	$title = "$appName - Błąd 403";
 	include("html/Begin.php");
-	$previous=$_SERVER['HTTP_REFERER'];
+	$previous = $_SERVER['HTTP_REFERER'];
 ?>
-<div class="container response-error-code">
-	<div class="col-xs-12 col-sm-12 col-md-12" >
-		<img src="img/403.png" alt="">
-		<h1>BŁĄD 403</h1>
-		<h2>Brak dostępu</h2>
-		<?php
-			if(isset($previous)){
-				echo '<a href="$previous">Powrót do poprzedniej strony</a>';
-			}else{
-				echo '<a href="index.php">Zapraszamy na stronę główną</a>';
-			}
-		?>
-	</div>
+<h1 style="text-align: center;">BŁĄD 404 - Brak dostępu</h1>
+<hr />
+<div style="text-align: center;">
+	<img src="img/403.png">
 </div>
-<?php 
+<?php
+	echo "<h3 style=\"text-align: center; margin-top: 10px;\">";
+	if (isset($previous)) {
+		echo '<a href="$previous">Powrót do poprzedniej strony</a>';
+	} else {
+		echo '<a href="index.php">Powrót do strony głównej</a>';
+	}
+	echo "</h3>";
 	include("html/End.php");
 ?>
