bd162108459a0b945a8830e44137cabf97ca4fc5
Michal Szura <michal.szura@gmail.com>
2014-06-03 01:52:07 +0200

    AdminEdit final

--- AdminEdit.php
+++ AdminEdit.php
@@ -39,7 +39,14 @@
 	<fieldset class="col-xs-12	col-sm-12	col-md-12">';
 
 	echo '<h4>Debug Mode</h4>';
-	echo 'Włączony?: <input type="checkbox" '.($xml->Debug == 1 ? "checked=checked" : "").'>';
+?>
+	<div class="form-group">
+		<label for="<?php echo strtolower($xml->Debug->getName());?>" class="col-xs-1	col-sm-1	col-md-1	control-label"><?php echo $xml->Debug->getName(); ?></label>
+			<div class="col-xs-4	col-sm-4	col-md-4">
+				<input type="checkbox" class="form-control" id="<?php echo strtolower($xml->Debug->getName());?>" name="<?php echo strtolower($xml->Debug->getName());?>" <?php if($xml->Debug == 1){echo 'checked=checked';}else{echo "";}?>>
+		</div>
+	</div><br>
+<?php
 	echo '<hr>';
 
 	echo '<h4>Dostępne Domeny</h4>';
@@ -48,48 +55,68 @@
 		<label>Dodaj Domenę
 			<input type="text" id="add_domain" />
 		</label>
-		<button class="button" id="add">Dodaj</button>
+		<a class="btn btn-default" id="add" href="#" role="button">Dodaj</a>
 	</p> 
-	<form action="" method="post">
-		<ul id="domains">';
+		<ul id="domainsList">';
 	foreach( $xml->Domains->children() as $child) {
-		$listitem_html = '<li>'.$child.'<input type="hidden" name="Domains[]" value="' . $child . '" /> '.'<a href="#" class="remove_domain">Usuń</a>'.'</li>';
+		$listitem_html = '<li>'.$child.'<input type="hidden" name="domains[]" value="' . $child . '" /> '.'</li>';
 		echo $listitem_html;
 	}
 	echo '</ul>
-		<input class="button" type="submit" value="Zapisz">
-	</form>';
+	';
 	echo '<hr>';
 
 	echo '<h4>Ustawienia E-mail</h4><br>';
-	foreach( $xml->Email->children() as $child) {
 ?>
 	<div class="form-group">
-		<label for="<?php echo strtolower($child->getName());?>" class="col-xs-1	col-sm-1	col-md-1	control-label"><?php echo $child->getName(); ?></label>
+		<label for="<?php echo strtolower($xml->Email->Adress->getName());?>" class="col-xs-1	col-sm-1	col-md-1	control-label"><?php echo $xml->Email->Adress->getName(); ?></label>
 			<div class="col-xs-4	col-sm-4	col-md-4">
-				<input type="text" class="form-control" id="<?php echo strtolower($child->getName());?>" name="nameEdit" value="<?php echo $child; ?>">
+				<input type="text" class="form-control" id="<?php echo strtolower($xml->Email->Adress->getName());?>" name="<?php echo strtolower($xml->Email->Adress->getName());?>" value="<?php echo $xml->Email->Adress; ?>">
+		</div>
+	</div><br>
+	<div class="form-group">
+		<label for="<?php echo strtolower($xml->Email->Password->getName());?>" class="col-xs-1	col-sm-1	col-md-1	control-label"><?php echo $xml->Email->Password->getName(); ?></label>
+			<div class="col-xs-4	col-sm-4	col-md-4">
+				<input type="password" class="form-control" id="<?php echo strtolower($xml->Email->Password->getName());?>" name="<?php echo strtolower($xml->Email->Password->getName());?>" value="<?php echo $xml->Email->Password; ?>">
+				<input id="pass2" type="checkbox" />Pokaż hasło
+		</div>
+	</div><br>
+	<div class="form-group">
+		<label for="<?php echo strtolower($xml->Email->Host->getName());?>" class="col-xs-1	col-sm-1	col-md-1	control-label"><?php echo $xml->Email->Host->getName(); ?></label>
+			<div class="col-xs-4	col-sm-4	col-md-4">
+				<input type="text" class="form-control" id="<?php echo strtolower($xml->Email->Host->getName());?>" name="<?php echo strtolower($xml->Email->Host->getName());?>" value="<?php echo $xml->Email->Host; ?>">
+		</div>
+	</div><br>
+	<div class="form-group">
+		<label for="<?php echo strtolower($xml->Email->Port->getName());?>" class="col-xs-1	col-sm-1	col-md-1	control-label"><?php echo $xml->Email->Port->getName(); ?></label>
+			<div class="col-xs-4	col-sm-4	col-md-4">
+				<input type="text" class="form-control" id="<?php echo strtolower($xml->Email->Port->getName());?>" name="<?php echo strtolower($xml->Email->Port->getName());?>" value="<?php echo $xml->Email->Port; ?>">
 		</div>
 	</div><br>
 <?php
-	}
 	echo '<hr>';
 
 	echo '<h4>Ustawienia Autoryzacji</h4><br>';
-	foreach( $xml->Authorization->children() as $child) {
 ?>
 	<div class="form-group">
-		<label for="<?php echo strtolower($child->getName());?>" class="col-xs-1	col-sm-1	col-md-1	control-label"><?php echo $child->getName(); ?></label>
+		<label for="<?php echo strtolower($xml->Authorization->UseCode->getName());?>" class="col-xs-1	col-sm-1	col-md-1	control-label"><?php echo $xml->Authorization->UseCode->getName(); ?></label>
 			<div class="col-xs-4	col-sm-4	col-md-4">
-				<input type="text" class="form-control" id="<?php echo strtolower($child->getName());?>" name="nameEdit" value="<?php echo $child; ?>">
+				<input type="checkbox" class="form-control" id="<?php echo strtolower($xml->Authorization->UseCode->getName());?>" name="<?php echo strtolower($xml->Authorization->UseCode->getName());?>" <?php if($xml->Authorization->UseCode == 1){echo 'checked=checked';}else{echo "";}?>>
+		</div>
+	</div><br>
+	<div class="form-group">
+		<label for="<?php echo strtolower($xml->Authorization->Code->getName());?>" class="col-xs-1	col-sm-1	col-md-1	control-label"><?php echo $xml->Authorization->Code->getName(); ?></label>
+			<div class="col-xs-4	col-sm-4	col-md-4">
+				<input type="password" class="form-control" id="<?php echo strtolower($xml->Authorization->Code->getName());?>" name="<?php echo strtolower($xml->Authorization->Code->getName());?>" value="<?php echo $xml->Authorization->Code; ?>">
+				<input id="code2" type="checkbox" />Pokaż hasło
 		</div>
 	</div><br>
 <?php
-	}
 	echo '<hr>';
 ?>
 <div class="form-group">
 			<span class="col-xs-3 col-sm-3	col-md-3">
-				<button type="submit" class="btn	btn-primary" name="submitButton" value="submit">Zapisz ustawienia</button>
+				<button type="submit" class="btn btn-primary" name="submitButton" value="submit">Zapisz ustawienia</button>
 			</span>
 		</div>
 	</fieldset>
--- js/AdminEdit.js
+++ js/AdminEdit.js
@@ -6,11 +6,11 @@ $(function () {
         
        listitem_html = '<li>';
        listitem_html += val;
-       listitem_html += '<input type="hidden" name="Domains[]" value="' + val + '" /> ';
+       listitem_html += '<input type="hidden" name="domains[]" value="' + val + '" /> ';
        listitem_html += '<a href="#" class="remove_domain">Usuń</a>'
        listitem_html += '</li>';
        
-        $('#domains').append(listitem_html);
+        $('#domainsList').append(listitem_html);
 
 		$( ".remove_domain" ).bind('click', function(e) {
 			e.preventDefault();
@@ -24,5 +24,32 @@ $(function () {
 		e.preventDefault();
 		$(this).parent().remove();
     });
+
+	$.toggleShowPassword = function (options) {
+        var settings = $.extend({
+            field: "#password",
+            control: "#toggle_show_password",
+        }, options);
+
+        var control = $(settings.control);
+        var field = $(settings.field)
+
+        control.bind('click', function () {
+            if (control.is(':checked')) {
+                field.attr('type', 'text');
+            } else {
+                field.attr('type', 'password');
+            }
+        })
+    };
     
+	$.toggleShowPassword({
+		field: '#code',
+		control: '#code2'
+	});
+	$.toggleShowPassword({
+		field: '#password',
+		control: '#pass2'
+	});
+
 });
