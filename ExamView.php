52da62a9212362270b09896d57707d726460a9f9
Konrad Welc <konrad1331@gmail.com>
2014-06-03 21:55:00 +0200

    .fix2

--- ExamView.php
+++ ExamView.php
@@ -13,7 +13,7 @@
 		return;
 	}
 	$user1 = unserialize($_SESSION['USER']);
-	if ($user1->getRight()!="examinator") {
+	if ($user1->getRight()!="examiner") {
 		include("html/AdminPanel.php");
 		
 		if (!isset($_GET['id'])) {
